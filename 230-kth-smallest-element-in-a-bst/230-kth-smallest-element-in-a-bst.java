a2f35d8e955bf3e60fde73d5fd90f7ea496cca82
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-02-28 11:04:27 +0300

    Time: 2 ms (34.05%), Space: 46.3 MB (5.90%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -15,16 +15,24 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        Queue<Integer> pq = new PriorityQueue<Integer>((a,b) -> b - a);
-        dfs(root,k,pq);
-        return pq.peek();
+        List<Integer> sorted = new ArrayList<>();
+        dfs(root,sorted);
+        
+        return sorted.get(k - 1);
     }
     
-    private void dfs(TreeNode node, int k, Queue<Integer> pq){
+    private void dfs(TreeNode node, List<Integer> list){
         if(node == null) return;
-        pq.add(node.val);
-        if(pq.size() > k ) pq.poll(); 
-        dfs(node.left, k, pq);
-        dfs(node.right,k,pq);
+        dfs(node.left,list);
+        list.add(node.val);
+        dfs(node.right,list);
     }
 }
+
+/*
+    Solution1: using maxHeap
+    
+    Solution2: using inorder traversal 
+
+
+*/
