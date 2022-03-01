802387776c7bdde1dff41394797dc7e877b0238d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-01 13:40:29 +0300

    Time: 2 ms (33.58%), Space: 48.2 MB (15.32%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -5,35 +5,36 @@ class Solution {
         
         directions[0] = new int[]{-1,0}; directions[1] = new int[]{0,1};
         directions[2] = new int[]{1,0}; directions[3] = new int[]{0,-1};
-        
-        Set<String> visited = new HashSet<>();
-        
-        dfs(image,sr,sc,image[sr][sc],newColor,visited);
+                
+        dfs(image,sr,sc,image[sr][sc],newColor);
         
         return image;
     }
     
-    private void dfs(int[][] image, int sr, int sc,int color,  int newColor,Set<String> visited){
-        String key = getKey(sr,sc);
-        if(visited.contains(key)) return;
-        visited.add(key);
-        if(image[sr][sc] == color)
+    private void dfs(int[][] image, int sr, int sc,int color,  int newColor){
+
+        if(image[sr][sc] == color && image[sr][sc] != newColor) 
+        // checks if it's not visited and same color with starting cell 
         {
-            image[sr][sc] = newColor;
+            image[sr][sc] = newColor; 
+            // changes its color, also implicitly marks the cell visited
       
             for(int[] dir : directions){
                 int i = sr+dir[0], j = sc + dir[1];
                 if(i < 0 || i >= image.length || j < 0 || j >= image[0].length) continue;
-                String k = getKey(i,j);
-                if(!visited.contains(k))
-                    dfs(image,i,j,color, newColor,visited);
+                dfs(image,i,j,color, newColor);
                 
             }
         }
         
     }
-    
-    private String getKey(int sr, int sc){
-        return sr+","+sc;
-    }
+
 }
+
+/*
+    1) First implementation using Visited set: 
+        - I used set to keep track of visited cells, but it is redundant becuase we are already changing 
+        visited cells to a new color, so we can keep track of visited cells implcitly.
+
+    2) Without using visited set:
+*/
