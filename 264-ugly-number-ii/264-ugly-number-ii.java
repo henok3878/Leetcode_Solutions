1194e0f06c9840e234c3a7e41634e98f107ae336
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-02 18:18:32 +0300

    Time: 89 ms (23.69%), Space: 61.3 MB (12.61%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,26 +1,23 @@
 class Solution {
     public int nthUglyNumber(int n) {
-       Queue<Long> pq = new PriorityQueue<>();
-       Set<Long> visited = new HashSet<>();
-       pq.add((long)1);
-       Long res = (long)1;
-       for(int i = 0; i < n; i++){
-            res = pq.poll();
-            long twos = res*2;
-            long threes = res*3;
-           long fives = res*5;
-            if(!visited.contains(twos)){
-                pq.add(twos);
-                visited.add(twos);
-            }
-            if(!visited.contains(threes)) {
-                pq.add(threes);
-                visited.add(threes);}
-            if(!visited.contains(fives)) {
-                pq.add(fives);
-                visited.add(fives);}
-       } 
+        PriorityQueue<Long> pq=new PriorityQueue<>();
+        HashMap<Long, Boolean> map=new HashMap<>();
+        int[] primes = new int[]{2,3,5};
+        pq.add((long)1);
+        map.put((long)1, true);
+        long res=0;
         
-       return res.intValue();
+        while(n > 0){
+            res = pq.poll();
+            for(int prime: primes){
+                Long nextUgly = res* prime;
+                if(!map.containsKey(nextUgly)){
+                    pq.add(nextUgly);
+                    map.put(nextUgly, true);
+                }
+            }
+            n--;
+        }      
+        return (int) res;
 	}
 }
