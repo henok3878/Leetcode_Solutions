64a034541d509867868abab70efedd6bc791c1e6
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-02 18:19:15 +0300

    Time: 14 ms (37.93%), Space: 47.8 MB (26.62%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.java
+++ 264-ugly-number-ii/264-ugly-number-ii.java
@@ -1,23 +1,17 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        PriorityQueue<Long> pq=new PriorityQueue<>();
-        HashMap<Long, Boolean> map=new HashMap<>();
-        int[] primes = new int[]{2,3,5};
-        pq.add((long)1);
-        map.put((long)1, true);
-        long res=0;
-        
-        while(n > 0){
-            res = pq.poll();
-            for(int prime: primes){
-                Long nextUgly = res* prime;
-                if(!map.containsKey(nextUgly)){
-                    pq.add(nextUgly);
-                    map.put(nextUgly, true);
-                }
-            }
-            n--;
-        }      
-        return (int) res;
+		if(n<=0) return 0;
+		int twosPointer=0,threesPointer=0,fivesPointer=0;
+		List<Integer> uglies = new ArrayList<Integer>();
+		uglies.add(1);
+		while(uglies.size() < n)
+		{
+			int nextUgly = Math.min(uglies.get(twosPointer)*2,Math.min(uglies.get(threesPointer)*3,uglies.get(fivesPointer)*5));
+			uglies.add(nextUgly);
+			if(uglies.get(twosPointer)*2==nextUgly) twosPointer++;
+			if(uglies.get(threesPointer)*3==nextUgly) threesPointer++;
+			if(uglies.get(fivesPointer)*5==nextUgly) fivesPointer++;
+		}
+		return uglies.get(uglies.size()-1);
 	}
 }
