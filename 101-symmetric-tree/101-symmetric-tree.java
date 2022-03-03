48260e135306a4b14765e01b4ffd6d1d4efa8bae
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-03 19:17:01 +0300

    Time: 1 ms (60.35%), Space: 42.6 MB (13.92%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,45 +15,16 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-       
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            List<Integer> currLevel = new ArrayList<>();
-            
-            for(int i = 0 ; i < size; i++){
-                TreeNode curr = queue.poll();
-                currLevel.add(curr.left == null ? -101 : curr.left.val);
-                if(curr.left != null) queue.add(curr.left);
-                currLevel.add(curr.right == null ? -101 : curr.right.val);
-                if(curr.right != null) queue.add(curr.right);
-                                                                                                }
-            if(!isSymmetric(currLevel)) return false;
-        }
-        
-        return true;
+       return isMirror(root,root);
     }
     
-    private boolean isSymmetric(List<Integer> list){
-        int st = 0, size = list.size();
-        while(st < size / 2){
-            if(list.get(st) != list.get(size - 1 - st)) return false;
-            st++;
+    private boolean isMirror(TreeNode left, TreeNode right){
+        if(left == null && right == null) return true;
+        else if(left == null || right == null) return false;
+        else if(left.val == right.val){
+            return isMirror(left.left, right.right) && isMirror(left.right, right.left);    
         }
-        return true;
+        return false;
+        
     }
-    
 }
-
-/*
-    Trick: Check the level below you not the level you are in
-        ex:       2   level 1
-                 / \  level 2
-                 \ /\ level 3 
-                 by default level 1 is always valid becuase it contains a single element 
-                 stand at level 1 and check if level 2 is valid if it is valid 
-                 stand at level 2 and check if level 3 is valid and keep doing 
-
-*/
