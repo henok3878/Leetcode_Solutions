c000472ab859cff1f9903e059f74fc13b83c1a97
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-03 18:18:58 +0300

    Time: 39 ms (95.89%), Space: 60 MB (93.83%) - LeetHub

--- 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.java
+++ 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.java
@@ -16,27 +16,25 @@ class Solution {
             graph.get(v).add(u);
         }
         
-        dfs(0, new HashSet<Integer>());
-        dfs2(0, new HashSet<Integer>());
+        dfs(0, -1);
+        dfs2(0, -1);
         return ans;
     }
     
-     public void dfs(int node, Set<Integer> visited) {
-        visited.add(node);
+     public void dfs(int node, int parent) {
         for (int child: graph.get(node))
-            if(!visited.contains(child)){
-                dfs(child, visited);
+            if (child != parent) {
+                dfs(child, node);
                 count[node] += count[child];
                 ans[node] += ans[child] + count[child];
             }
     }
 
-    public void dfs2(int node, Set<Integer> visited) {
-        visited.add(node);
+    public void dfs2(int node, int parent) {
         for (int child: graph.get(node))
-            if (!visited.contains(child)) {
-                ans[child] = ( ans[node] - count[child] ) + N - count[child];
-                dfs2(child, visited);
+            if (child != parent) {
+                ans[child] = ans[node] - count[child] + N - count[child];
+                dfs2(child, node);
             }
     }
 }
