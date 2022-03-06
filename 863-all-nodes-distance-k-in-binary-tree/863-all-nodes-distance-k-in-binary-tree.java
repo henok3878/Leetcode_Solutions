c91234cf9a5cdde1d305dcd1871b190ef2ee8550
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-06 21:22:45 +0300

    Time: 16 ms (63.89%), Space: 44.1 MB (6.16%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -21,7 +21,7 @@ class Solution {
             int size = queue.size();
             for(int i = 0; i < size; i++){
                 int curr = queue.poll();
-                if(dist == k) ans.add(curr);
+                if(dist == k){ ans.add(curr); continue;}
                 for(int adj : graph.get(curr)){
                     if(!visited.contains(adj)){
                         queue.add(adj);
