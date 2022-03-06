00f9e38e517c92d53bb0b29d5b29dd934a7b131f
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-06 18:46:39 +0300

    Time: 5 ms (90.20%), Space: 47.1 MB (35.97%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,54 @@
+class Solution {
+	public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        
+        List<List<Node>> graph = new ArrayList<>();
+        for(int city = 0; city < n ; city++) graph.add(new ArrayList<>());
+        
+        for(int[] flight : flights){
+            int u = flight[0], v = flight[1], price = flight[2];
+            graph.get(u).add(new Node(v,price));
+        }
+
+		Queue<Node> q = new LinkedList<Node>();
+		q.add(new Node(src, 0));
+		int[] minDist = new int[n];
+		Arrays.fill(minDist, Integer.MAX_VALUE);
+		int count = -1;
+
+        while(!q.isEmpty()) {
+			if(count == k) {
+				break;
+			}
+			for(int i = q.size() - 1; i >= 0; --i) {
+				Node curr = q.poll();
+				int node = curr.city;
+				int weight = curr.price;
+                for(Node adj : graph.get(node)){
+                    int city = adj.city;
+                    int w = weight + adj.price;
+                    if (w < minDist[city]) {
+                        minDist[city] = w;
+                        // adj.city = city;
+                        // adj.price = w;
+                        // q.add(adj);
+                        q.add(new Node(city, w));
+                    }
+                }
+
+			}
+			count++;
+		}
+		return minDist[dst] != Integer.MAX_VALUE ? minDist[dst] : -1;
+	}
+}
+class Node{
+    int city;
+    int price;
+    Node(int city, int price){
+        this.city = city;
+        this.price = price;
+    }
+    public String toString(){
+        return "(" + city + ":" + price + ")";
+    }
+}
