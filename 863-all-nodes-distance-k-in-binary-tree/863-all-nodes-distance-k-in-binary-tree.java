cebe22d202b862a189149b3bcfbc5f9dc5bdad20
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-06 21:22:13 +0300

    Time: 33 ms (8.41%), Space: 44.2 MB (6.16%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        buildGraph(root, graph);
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
+        visited.add(target.val);
+        queue.add(target.val);
+        int dist = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                int curr = queue.poll();
+                if(dist == k) ans.add(curr);
+                for(int adj : graph.get(curr)){
+                    if(!visited.contains(adj)){
+                        queue.add(adj);
+                        visited.add(adj);
+                    }
+                }
+                
+            }
+            dist++;
+
+        }
+        
+        return ans;
+    }
+    
+    private void buildGraph(TreeNode root, Map<Integer,List<Integer>> graph){
+        if(root == null) return;
+        List<Integer> rootAdjs = graph.getOrDefault(root.val,new ArrayList<>());
+        if(root.left != null){
+             List<Integer> leftAdjs = graph.getOrDefault(root.left.val, new ArrayList<>());         rootAdjs.add(root.left.val);
+             leftAdjs.add(root.val);
+            graph.put(root.left.val, leftAdjs);
+        }
+        if(root.right != null){
+            List<Integer> rightAdjs = graph.getOrDefault(root.right.val, new ArrayList<>());        rightAdjs.add(root.val);
+            rootAdjs.add(root.right.val);
+            graph.put(root.right.val,rightAdjs);
+        }
+        graph.put(root.val,rootAdjs);
+        buildGraph(root.left,graph);
+        buildGraph(root.right,graph);
+        
+    }
+}
