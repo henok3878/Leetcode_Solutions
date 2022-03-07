2ffdd105614bd2003b687c3fe56c778d882d66e7
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-07 13:52:50 +0300

    Time: 5 ms (73.31%), Space: 42.1 MB (50.23%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] lastIdx = new int[26];
+        for(int i = 0; i < s.length() ; i++) lastIdx[s.charAt(i) - 'a'] = i;
+        
+        List<Integer> ans = new ArrayList<>();
+        
+        int max = -1;
+        int st = 0;
+        for(int i = 0; i < s.length(); i++){
+            max = Math.max(lastIdx[s.charAt(i) - 'a'], max);
+            if(max == i){
+                ans.add(max - st + 1);
+                st = i + 1;
+                max = -1;
+            }
+        }
+        return ans;
+    }
+}
+/*
+st: 1:35 
+
+"ababcbacadefegdehijhklij"
+ 
+
+[a: 8, b:5 ]
+
+*/
