f61f963c8e522f599b13d7065547b73a696f9336
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-07 21:19:11 +0300

    Time: 6 ms (45.90%), Space: 41.9 MB (46.81%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String reorganizeString(String s) {
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++) count[s.charAt(i) - 'a']++;
+        Queue<Letter> pq = new PriorityQueue<>((a,b) -> b.freq - a.freq);
+        for(int i = 0; i < count.length; i++)
+            if(count[i] > 0) pq.add(new Letter((char)(i + 'a'),count[i]));
+    
+        StringBuilder sb = new StringBuilder();
+        
+        Letter cool = null;
+        while(!pq.isEmpty()){
+            Letter curr = pq.poll();
+            curr.freq -= 1;
+            sb.append(curr.c);
+            if(cool != null && cool.c != sb.charAt(sb.length() - 1)){
+                pq.add(cool);
+                cool = null;
+            }
+            if(curr.freq > 0) cool = curr; 
+        }
+        
+        return (cool != null) ? "" : sb.toString();
+    }
+}
+class Letter{
+    char c;
+    int freq;
+    Letter(char c , int freq){
+        this.c = c; this.freq = freq;
+    }
+    public String toString(){return "(" + c + "," + freq +")";}
+}
+/*
+st: 8:46
+
+
+*/
