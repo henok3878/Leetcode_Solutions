69defe46d8eb64c6c9b025279fad52d5aa6406fa
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-07 20:21:26 +0300

    Time: 14 ms (37.34%), Space: 54.4 MB (45.28%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -3,17 +3,17 @@ class Solution {
         int m = people.length, n = people[0].length;
         
         Arrays.sort(people,(a,b) -> (b[0] == a[0]) ? a[1] - b[1] : b[0] - a[0]);
-        List<List<Integer>> temp = new ArrayList<>();
+        List<int[]> temp = new ArrayList<>();
         
         for(int[] person : people){
             int idx = person[1];
-            temp.add(idx,Arrays.asList(person[0],person[1]));
+            temp.add(idx,new int[]{person[0],person[1]});
         }
         
         int[][] ans = new int[m][n];
         int idx = 0;
-        for(List<Integer> item : temp)
-            ans[idx++] = new int[]{item.get(0),item.get(1)};
+        for(int[] item : temp)
+            ans[idx++] = item;
         
         return ans;   
     }
