51ca254e6a5463f93805bd415b887247ecd1581c
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-07 16:09:20 +0300

    Time: 11 ms (40.17%), Space: 47.7 MB (12.66%) - LeetHub

---
+++ 1529-minimum-suffix-flips/1529-minimum-suffix-flips.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minFlips(String target) {
+        int n = target.length();
+        char[] init = new char[n];
+        Arrays.fill(init,'0');
+        
+        int flips = 0;
+        boolean isFliped = false;
+        for(int i = 0; i < n; i++){
+            if(isFliped){
+                if(target.charAt(i) == init[i]){
+                    flips++;
+                    isFliped = false;
+                }
+            }else if(target.charAt(i) != init[i]){
+                isFliped = true;
+                flips++;
+            }
+        }
+        return flips;
+    }
+}
+
+/*
+st: 3:45 
+
+
+target: 10111
+init  : 00000   
+        00111
+        11000
+        10111
+
+*/
