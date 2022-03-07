cf22dfa5e0e22e85ab592a5420c81ad435337644
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-07 20:11:56 +0300

    Time: 16 ms (29.48%), Space: 55.3 MB (5.67%) - LeetHub

---
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+        int m = people.length, n = people[0].length;
+        
+        Arrays.sort(people,(a,b) -> (b[0] == a[0]) ? a[1] - b[1] : b[0] - a[0]);
+        List<List<Integer>> temp = new LinkedList<>();
+        
+        for(int[] person : people){
+            int idx = person[1];
+            temp.add(idx,Arrays.asList(person[0],person[1]));
+        }
+        
+        int[][] ans = new int[m][n];
+        int idx = 0;
+        for(List<Integer> item : temp)
+            ans[idx++] = new int[]{item.get(0),item.get(1)};
+        
+        return ans;   
+    }
+}
