82cd3913fbffff710b58e92d6d0613449da394fe
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-08 18:31:03 +0300

    Time: 511 ms (5.06%), Space: 43.5 MB (61.80%) - LeetHub

--- 649-dota2-senate/649-dota2-senate.java
+++ 649-dota2-senate/649-dota2-senate.java
@@ -1,33 +1,31 @@
 class Solution {
     public String predictPartyVictory(String senate) {
         char[] senates = senate.toCharArray();
-        int n = senate.length();
-        int radiant = 0, dire = 0, start = 0;
+        int n = senates.length;
+        int radiant = 0, dire = 0;
         
         for(int i = 0; i < n;i++) {
             if(senates[i] == 'R') radiant++;
             else dire++;
         }
-        
+        int start = 0;
         while(radiant > 0 && dire > 0){
-            while (senates[start] != 'R' && senates[start] != 'D') {
-                start = (start + 1) % senates.length;
+            if(senates[start] != 'R' && senates[start] != 'D'){
+                start = (start + 1) % n;
             }
-            
-            char ban = 'R';
-            if (senates[start] == 'R') {
-                ban = 'D';
-                dire--;
+            else{
+                char currBanned = 'R'; 
+                if(senates[start] == 'R'){
+                    currBanned = 'D';
+                    dire--;
+                }else radiant--;
+                int nxt = (start + 1) % n;
+                start = nxt;
+                while(senates[nxt] != currBanned){
+                    nxt = (nxt + 1) % n;
+                }
+                senates[nxt] = ' ';
             }
-            else radiant--;
-            
-            int idx = (start + 1) % senates.length;
-            while (senates[idx] != ban) {
-                idx = (idx + 1) % senates.length;
-            }
-            
-            senates[idx] = ' ';
-            start = (start + 1) % senates.length;
         }
         
         return (radiant > 0) ? "Radiant" : "Dire";
@@ -36,5 +34,5 @@ class Solution {
 
 /*
 st: 3:37 
-
+sub: 4:16 
 */
