566ec0f5d4b0938e5f0fba83311c5a8b46e59415
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-08 14:45:53 +0300

    Time: 1 ms (93.42%), Space: 41.4 MB (44.45%) - LeetHub

--- 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.java
+++ 1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/1414-find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k.java
@@ -1,28 +1,21 @@
 class Solution {
     public int findMinFibonacciNumbers(int k) {
-        List<Integer> fibList = new ArrayList<>();
-        generateFib(k,fibList);
-        int count = 0, i = fibList.size() - 1;
-        while(i >= 0 && k > 0){
-            if(k - fibList.get(i) >= 0){
-                k -= fibList.get(i);
+        int prev = 1, curr = 1;
+        while(curr + prev <= k){
+            curr += prev;
+            prev = curr - prev;
+        }
+        int count = 0;
+
+        while(k > 0){
+            if(k - curr >= 0){
+                k -= curr; 
                 count++;
             }
-            i--;
+            prev = curr - prev;
+            curr -= prev;
         }
-        return count;        
-    }
-    
-    private void generateFib(int n , List<Integer> fibList){
-        fibList.add(1);fibList.add(1);
-        int i = 1, j = 1;
         
-        while(i+j <= n){
-            int temp = j;
-            j = i + j;
-            i = temp;
-            fibList.add(j);
-            
-        }
+        return count;
     }
 }
