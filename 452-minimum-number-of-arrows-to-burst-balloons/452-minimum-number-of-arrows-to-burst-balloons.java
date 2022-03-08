419cbfc4e63b1e1ee2723992f2d4c998a2a7d42c
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-08 21:24:33 +0300

    Time: 144 ms (10.66%), Space: 104.2 MB (65.28%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,24 +1,34 @@
 class Solution {
-    
     public int findMinArrowShots(int[][] points) {
-        
-        Arrays.sort(points, (a, b) -> Integer.compare(a[0], b[0]));
-        Stack<List<Integer>> stack = new Stack();
-        for(int i = 0; i < points.length; i++){
-            List<Integer> currInterval = new ArrayList(Arrays.asList(points[i][0],points[i][1]));
-            if(!stack.isEmpty()){
-                List<Integer> recentInterval = stack.peek();
-                if(currInterval.get(0) <= recentInterval.get(1)){
-                    int st = Math.max(currInterval.get(0),recentInterval.get(0));
-                    int end = Math.min(currInterval.get(1),recentInterval.get(1));
-                    currInterval = new ArrayList(Arrays.asList(st,end));
+        Arrays.sort(points,(a,b)-> Integer.compare(a[0],b[0]));
+        Stack<int[]> stack = new Stack();
+        for(int[] interval : points){
+            if(stack.isEmpty()) stack.push(interval);
+            else{
+                int[] top = stack.peek();
+                if(interval[0] <= top[1]){
+                    int end = Math.min(top[1],interval[1]);
                     stack.pop();
-                }
+                    stack.push(new int[]{interval[0],end});
+                }else if(interval[0] > top[1]) stack.push(interval);
             }
-            stack.push(currInterval);
-            
         }
         return stack.size();
-        
     }
 }
+
+/*
+st: 9:00
+sub: 9:20 
+    
+
+[[10,16],[2,8],[1,6],[7,12]]
+
+1  -  6 
+   2  - 8 
+      7  -  12
+        10  -   16
+
+
+
+*/
