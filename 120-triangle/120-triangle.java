8a8bbfbc02967e0d4edba538b4ceb1ab42275488
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-10 16:08:45 +0300

    Time: 2 ms (93.93%), Space: 41.9 MB (62.36%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -1,18 +1,20 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
+        int levels = triangle.size();
+        int lastRow = triangle.get(levels-1).size();
+        int[][] dp = new int[levels][lastRow + 1];
+        for(int[] row : dp) Arrays.fill(row, Integer.MAX_VALUE);
+        dp[0][1] = triangle.get(0).get(0);
         
-        if(triangle.size() == 0) return 0;
-        
-        int rows = triangle.size();
-
-        int [] dp = new int[rows+1];
-        
-        for(int i=rows-1; i>=0; i--){
-            for(int j=0; j<=i; j++){
-    dp[j] = Math.min(dp[j], dp[j+1]) + triangle.get(i).get(j);
+        for(int i = 1;i < levels; i++){
+            for(int j = 1; j <= i + 1;j++){
+                dp[i][j] = triangle.get(i).get(j-1) + Math.min(dp[i-1][j], dp[i-1][j-1]);
             }
         }
         
-        return dp[0];
+        int ans = Integer.MAX_VALUE;
+        for(int num : dp[levels-1]) ans = Math.min(ans,num);
+        
+        return ans;
     }
 }
