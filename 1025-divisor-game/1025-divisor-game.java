bfd3aa97a70deb680955b3cac24a300e0eab4b02
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-10 07:30:28 +0300

    Time: 6 ms (13.45%), Space: 40.5 MB (47.53%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,18 +1,30 @@
 class Solution {
     public boolean divisorGame(int n) {
-        return helper(n,true);
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(n,true,dp);
     }
-    private boolean helper(int n, boolean turn){
+    private boolean helper(int n, boolean turn,int[] dp){
         if(n == 2) return turn;
         else if(n < 2) return !turn;
+        else if(dp[n] != -1) return (dp[n] == 1) ? turn : !turn;
         
         for(int x = 1; x < n; x++){
-            if(n % x == 0){
-                if(helper(n - x,!turn) == turn) return turn;
-                else return !turn;
+            if(n % x == 0 && helper(n - x,!turn,dp) == turn){
+                 dp[n] = 1;
+                 return turn;
             }
+            
         }
-        
+        dp[n] = 0;   
         return !turn;
     }
+    
 }
+
+/*
+    Arrays.sort(intervals, (a,b)-> a[0] - b[0]); // increasing, based on st 
+    
+
+
+*/
