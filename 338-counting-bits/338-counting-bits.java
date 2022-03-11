30714564d71a52ce245578b1c9c4fdb42e21d756
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-11 23:55:46 +0300

    Time: 5 ms (28.29%), Space: 48.1 MB (46.27%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,17 +1,21 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
+        Arrays.fill(ans,-1);
         
-        for(int i = 0; i <= n; i++) ans[i] = helper(i);
+        for(int i = 0; i <= n; i++) ans[i] = helper(i,ans);
         
         return ans;
     }
     
-    private int helper(int n){
+    private int helper(int n,int[] ans){
         if(n == 0) return 0;
         else if(n == 1) return 1;
+        else if(ans[n] != -1) return ans[n];
         
-        return (n %2 == 0) ? helper(n/2) : 1 + helper(n/2);
+        int res = (n %2 == 0) ? helper(n/2,ans) : 1 + helper(n/2,ans);
+        ans[n] =res;
+        return res;
     }
 }
 
