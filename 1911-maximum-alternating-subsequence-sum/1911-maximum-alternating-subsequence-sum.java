adab5dc7519a07cd3596daf6d30e1ececcc0fa1a
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-15 00:06:25 +0300

    Time: 10 ms (57.04%), Space: 104.1 MB (58.52%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.java
@@ -3,9 +3,9 @@ class Solution {
         int n = nums.length;
         long evenMax = 0, oddMax = 0;
         for(int i = 0; i < n; i++){
-            long odd = oddMax;
+            long prevOdd = oddMax;
             oddMax = Math.max(oddMax,evenMax - nums[i]); 
-            evenMax = Math.max(Math.max(nums[i],evenMax),odd + nums[i]);
+            evenMax = Math.max(Math.max(nums[i],evenMax),prevOdd + nums[i]);
         }
         
         return evenMax;
