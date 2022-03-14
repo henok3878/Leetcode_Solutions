f6837fcddbfc210ba592c05b487211c8f578fdf9
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-14 23:30:22 +0300

    Time: 3 ms (84.37%), Space: 41.9 MB (40.58%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        if(stones == null || stones.length == 0) return 0;
+        
+        int sum = 0, half_sum;
+        for(int stone: stones) sum += stone;
+        half_sum = sum / 2;
+
+        int dp[] = new int[half_sum + 1];
+        for(int i=0; i<stones.length; i++) {
+            for(int w=half_sum; w>=stones[i]; w--) {
+                dp[w] = Math.max(dp[w], dp[w-stones[i]] + stones[i]);
+            }
+        }
+
+        return sum - 2 * dp[half_sum];
+    }
+}
