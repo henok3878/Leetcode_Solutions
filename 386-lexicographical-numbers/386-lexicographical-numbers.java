0973b3c060a55486f3880ca315dbf7ebb22f98ff
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-16 11:58:46 +0300

    Time: 8 ms (43.23%), Space: 57.2 MB (18.72%) - LeetHub

--- 386-lexicographical-numbers/386-lexicographical-numbers.java
+++ 386-lexicographical-numbers/386-lexicographical-numbers.java
@@ -1,65 +1,21 @@
 class Solution {
-    
-    TrieNode root;
-    
     public List<Integer> lexicalOrder(int n) {
-        root = new TrieNode();
-        
-        /*
-            whole for loop: O(n*len(n)) 
-                -> len(n) <= 5, becuase 1 <= n <= 5*10^4 
-                O(n*5) = 
-                O(n)
-            
-        */
-        for(int num = 1; num <= n; num++)
-            insert(num); // O(len(n))
-        
         List<Integer> ans = new ArrayList<>();
         
-        /*
-            O(n)
-        */
-        traverseTrie(ans,root);
+        for(int i = 1; i < 10; i++)
+            dfs(i,n,ans);
+        
         return ans;
         
     }
     
-    private void traverseTrie(List<Integer> ans, TrieNode node){
-        if(node == null) return;
-        else if(node.isEnd) ans.add(node.data);
+    private void dfs(int curr, int n, List<Integer> ans){
+        if(curr > n) return;
+        
+        ans.add(curr);
         for(int i = 0; i < 10; i++){
-            if(node.children[i] != null){
-                traverseTrie(ans,node.children[i]);
-            }
+            dfs(10*curr + i,n,ans);
         }
     }
     
-    private void insert(int num){
-        String numStr = num + "";
-        TrieNode temp = root;
-        for(int i = 0; i < numStr.length(); i++){
-            int charIdx = numStr.charAt(i) -'0';
-            if(temp.children[charIdx] == null)
-                temp.children[charIdx] = new TrieNode();
-            temp = temp.children[charIdx];
-        }
-        temp.isEnd = true;
-        temp.data = num;
-    }
 }
-
-class TrieNode{
-    boolean isEnd;
-    TrieNode[] children;
-    int data;
-    public TrieNode(){
-        children = new TrieNode[10];
-    }
-    
-}
-
-/*
-    
-
-*/
