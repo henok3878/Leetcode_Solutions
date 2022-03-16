74bcfd82edf781e48e596617a8c0ddd4909073bd
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-16 11:02:24 +0300

    Time: 1 ms (73.49%), Space: 42.2 MB (36.26%) - LeetHub

---
+++ 1023-camelcase-matching/1023-camelcase-matching.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Boolean> camelMatch(String[] queries, String pattern) {
+        
+        List<Boolean> ans = new ArrayList<>();
+        int pLen = pattern.length();
+        for(String query : queries){
+            int qPtr = 0; // specifiy idx in query 
+            int pPtr = 0;
+            int qLen = query.length();
+            while(pPtr < pLen && qPtr < qLen){
+                char queryChar = query.charAt(qPtr);
+                char patternChar = pattern.charAt(pPtr);
+                if(queryChar == patternChar){
+                    qPtr++; 
+                    pPtr++;
+                }
+                else if(Character.isLowerCase(queryChar)) qPtr++;
+                else break;
+            }
+            while(qPtr < qLen && Character.isLowerCase(query.charAt(qPtr))) qPtr++;
+            if(pPtr == pLen && qPtr == qLen) ans.add(true);
+            else ans.add(false);
+        
+        }
+        
+        return ans;
+    }
+}
+
+/*
+Time Complexity: O(q*l), where: q is num of queries    
+                              : l is max length of a query in queries 
+                    
+
+
+*/
