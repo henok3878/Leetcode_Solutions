d7a3b56414ff0c7ca6554cc61ab427c8dc3c8881
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-18 01:22:44 +0300

    Time: 31 ms (55.38%), Space: 127.1 MB (52.93%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,46 +1,37 @@
 class Solution {
     int[][] directions = {{1,0},{0,1},{-1,0},{0,-1}};
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        List<List<int[]>> grid2Islands = new ArrayList<>();
+
         int m1 = grid1.length, n1 = grid1[0].length;
         int m2 = grid2.length, n2 = grid2[0].length;
         
+        int count = 0;
+        
         for(int i = 0; i < m2; i++){
             for(int j = 0; j < n2; j++){
-                List<int[]> island = new ArrayList<>();
-                if(grid2[i][j] == 1){
-                    dfs(i,j,grid2,island);
-                    grid2Islands.add(island);
-                }
+                if(grid2[i][j] == 1 && grid1[i][j] == 1)
+                    count += dfs(i,j,grid1,grid2) ? 1 : 0;
             }
-        }
-        
-        int count = 0;
-        for(List<int[]> island : grid2Islands){
-            if(dfs2(0,island,grid1)) count++;
         }
         
         return count;
         
     }
     
-    private void dfs(int r, int c, int[][] grid, List<int[]> path){
-        path.add(new int[]{r,c});
-        grid[r][c] = -1; // -1 representing visited cells 
+    private boolean dfs(int r, int c, int[][] grid1,int[][] grid2){
+        if(r < 0 ||  c < 0 || r >= grid2.length || c >= grid2[0].length || grid2[r][c] != 1) return true;
+        else if(grid1[r][c] != 1) return false;
+        
+        grid2[r][c] = -1; // -1 representing visited cells 
+        
+        boolean res = true;
         for(int[] dir : directions){
             int x = r + dir[0], y = c + dir[1];
-            if(x < 0 ||  y < 0 || x >= grid.length || y >= grid[0].length || grid[x][y] != 1) continue;
-            dfs(x,y,grid,path);
+            res &= dfs(x,y,grid1,grid2);
         }
+        return res;
     }
     
-    private boolean dfs2(int cellIdx, List<int[]> island, int[][] grid){
-        if(cellIdx == island.size()) return true;
-        int[] currCell = island.get(cellIdx);
-        if(grid[currCell[0]][currCell[1]] == 0) return false;
-        
-        return dfs2(cellIdx + 1, island,grid);
-    }
 }
 
 /*
@@ -54,4 +45,7 @@ class Solution {
     traversing the grid = O(Node + Edges)
                         = O(M*N + M*N)
                         = O(M*N)
+                        
+Optimization: 
+    - Remove 2nd step (traversal) 
 */
