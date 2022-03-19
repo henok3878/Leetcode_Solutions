b1dfaff35493993371f80d016ddb0c46ecc3e02d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-19 23:58:37 +0300

    Time: 3 ms (75.65%), Space: 65.7 MB (10.56%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,48 +1,29 @@
 class Solution {
-    
-    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
-        
     public int countServers(int[][] grid) {
-        
         int m = grid.length, n = grid[0].length;
-        int ans = 0;
+        
+        int[] rows = new int[m];
+        int[] cols = new int[n];
+        int servers = 0;
         
         for(int i = 0; i < m; i++){
-            for(int j = 0;j < n ;j++){
-                
+            for(int j = 0; j < n ; j++){
                 if(grid[i][j] == 1){
-                    int res = dfs(i,j,grid);
-                    ans += (res > 1 ) ? res : 0;
+                    rows[i]++;
+                    cols[j]++;
+                    servers++;
                 }
             }
         }
         
-        return ans;
-        
-    }
-    
-    
-    private int dfs(int i , int j, int[][] grid){
-        int m = grid.length; int n = grid[0].length;
-        if(i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) return 0;
-        int res = 1;
-        grid[i][j] = 0; // mark visited 
-        
-        for(int y = 0; y < n; y++){
-            res += dfs(i,y,grid);
-        }
-        for(int x = 0; x < m; x ++){
-            res += dfs(x,j,grid);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n ;j++){
+                if(grid[i][j] == 1 && rows[i] == 1 && cols[j] == 1){
+                    servers--;
+                }
+            }
         }
         
-        return res;
-        
+        return servers;
     }
 }
-
-/*
-    1st Appr: 
-        Do dfs on each cell that contains 1 
-
-
-*/
