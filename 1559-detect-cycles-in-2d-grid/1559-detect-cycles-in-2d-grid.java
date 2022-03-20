27c702ef6e727b7bc6f054e391dcd5241e30d048
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-21 02:24:46 +0300

    Time: 32 ms (55.60%), Space: 140 MB (50.31%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -3,33 +3,30 @@ class Solution {
     int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public boolean containsCycle(char[][] grid) {
         int m = grid.length, n = grid[0].length;
-        int[][] visited = new int[m][n];
-        for(int i = 0; i < m; i++) Arrays.fill(visited[i],-1);
+        boolean[][] visited = new boolean[m][n];
             
         for(int i = 0; i < m; i++){
             for(int j = 0;j < n; j++){
-                if(visited[i][j] == -1 && isCycleFound(i,j,i,j,visited,grid))
+                if(!visited[i][j] && isCycleFound(i,j,i,j,visited,grid))
                     return true;
             }
         }
         return false;
     }
     
-    private boolean isCycleFound(int currX,int currY, int parX,int parY, int[][] visited, char[][] grid){
-        int n = grid[0].length;
-        int currId = currX*n + currY;
-        int parId = parX*n + parY;
+    private boolean isCycleFound(int currX,int currY, int parX,int parY, boolean[][] visited, char[][] grid){
+
         if(currX < 0 || currY < 0 || currX >= grid.length || currY >= grid[0].length || grid[parX][parY] != grid[currX][currY])
             return false;
         
-        if(visited[currX][currY] != -1 && visited[parX][parY] != currId) return true;
-        else if(visited[currX][currY] != -1) return false;
+        if(visited[currX][currY] && grid[currX][currY] == grid[parX][parY]) return true;
+        else if(visited[currX][currY]) return false;
         
-        visited[currX][currY] = parId;
+        visited[currX][currY] = true;
         
         for(int[] dir : directions){
             int x = currX + dir[0], y = currY + dir[1];
-            if(isCycleFound(x,y,currX,currY,visited,grid)) return true;
+            if(!( x == parX && y == parY) && isCycleFound(x,y,currX,currY,visited,grid)) return true;
         }
         
         return false;
