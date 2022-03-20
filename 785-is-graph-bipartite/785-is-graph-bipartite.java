0a603a146420e84bf5a9fcb77f5de88f64fd9b8c
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-21 00:39:05 +0300

    Time: 3 ms (34.64%), Space: 53.9 MB (32.42%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,27 +1,54 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int[] visited = new int[n];
+        UnionFind uf = new UnionFind(n);
         
-        int pColor = -1;
-        for(int node = 0; node < n; node++){
-            if(visited[node] == 0 && !dfs(node,pColor,graph,visited))
-                return false;
-        }
-        return true;
-    }
-    
-    private boolean dfs(int node, int pColor, int[][] graph, int[] visited){
-        int currColor = pColor * -1;
-        visited[node] = currColor;
-        for(int adj : graph[node]){
-            if(visited[adj] != 0) {
-                if(visited[adj] == currColor) return false; 
-                continue;
+        for(int i = 0;i < n; i++){
+            
+            for(int j = 0;j < graph[i].length; j++){
+                
+                if(uf.isConnected(i,graph[i][j])) return false;
+                uf.union(graph[i][0], graph[i][j]);
             }
-            if(!dfs(adj,currColor,graph,visited)) return false;
         }
-        
         return true;
     }
 }
+
+class UnionFind{
+    
+    int[] parents;
+    int[] ranks;
+    
+    public UnionFind(int size){
+        parents = new int[size];
+        ranks = new int[size];
+        for(int i = 0;i < size; i++) parents[i] = i;
+    }
+    
+    public int find(int elem){
+        if(parents[elem] == elem) return elem;
+        return parents[elem] = find(parents[elem]);
+    }
+    
+    public void union(int elem1, int elem2){
+        int parent1 = find(elem1);
+        int parent2 = find(elem2);
+        
+        if(parent1 == parent2) return;
+        if(ranks[parent1] > ranks[parent2])
+            parents[parent2] = parent1;
+        else if(ranks[parent1] < ranks[parent2])
+            parents[parent1] = parent2;
+        else{
+            parents[parent2] = parent1;
+            ranks[parent1]++;
+        }
+    }
+    
+    public boolean isConnected(int elem1, int elem2){
+        return find(elem1) == find(elem2);
+    }
+
+}
+
