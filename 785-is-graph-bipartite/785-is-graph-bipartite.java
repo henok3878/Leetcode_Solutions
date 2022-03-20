383e511bef2ead443ee70d6a30dab0848227978b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-21 00:48:01 +0300

    Time: 1 ms (84.50%), Space: 52.4 MB (71.18%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,54 +1,23 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        UnionFind uf = new UnionFind(n);
         
-        for(int i = 0;i < n; i++){
-            
-            for(int j = 0;j < graph[i].length; j++){
-                
-                if(uf.isConnected(i,graph[i][j])) return false;
-                uf.union(graph[i][0], graph[i][j]);
-            }
+        int[] colors = new int[n+1];
+        colors[n] = -1;
+        
+        for(int i = 0; i < n; i++){
+            if(colors[i] == 0 && !dfs(i,n,colors,graph)) return false;    
         }
+        
+        return true;
+    }
+    
+    private boolean dfs(int n,int p, int[] colors, int[][] graph){
+        if(colors[n] != 0) return colors[p] != colors[n];
+
+        colors[n] = -1 * colors[p]; // mark visited and also color the node
+        for(int adj : graph[n])
+            if(!dfs(adj,n,colors,graph)) return false;
         return true;
     }
 }
-
-class UnionFind{
-    
-    int[] parents;
-    int[] ranks;
-    
-    public UnionFind(int size){
-        parents = new int[size];
-        ranks = new int[size];
-        for(int i = 0;i < size; i++) parents[i] = i;
-    }
-    
-    public int find(int elem){
-        if(parents[elem] == elem) return elem;
-        return parents[elem] = find(parents[elem]);
-    }
-    
-    public void union(int elem1, int elem2){
-        int parent1 = find(elem1);
-        int parent2 = find(elem2);
-        
-        if(parent1 == parent2) return;
-        if(ranks[parent1] > ranks[parent2])
-            parents[parent2] = parent1;
-        else if(ranks[parent1] < ranks[parent2])
-            parents[parent1] = parent2;
-        else{
-            parents[parent2] = parent1;
-            ranks[parent1]++;
-        }
-    }
-    
-    public boolean isConnected(int elem1, int elem2){
-        return find(elem1) == find(elem2);
-    }
-
-}
-
