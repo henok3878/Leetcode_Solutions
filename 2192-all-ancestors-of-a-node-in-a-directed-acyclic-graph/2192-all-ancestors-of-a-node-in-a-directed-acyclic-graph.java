baeb17bd4ad6abb718c55c9c32beb88768438974
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-23 02:33:14 +0300

    Time: 84 ms (91.92%), Space: 64.4 MB (95.53%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,42 +1,29 @@
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
         Map<Integer,List<Integer>> graph = new HashMap<>();
-        for(int i = 0;i < n; i++) graph.put(i, new ArrayList<>());
-        int[] indegree = new int[n];
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0;i < n; i++) {
+            ans.add(new ArrayList<>());
+            graph.put(i, new ArrayList<>());
+        }
         
         for(int[] edge : edges){
-            indegree[edge[1]]++;
             graph.get(edge[0]).add(edge[1]);
         }
         
-        Queue<Integer> queue = new LinkedList<>();
-        
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0) queue.add(i);
-        }
-        Map<Integer,Set<Integer>> ancestor = new HashMap<>();
-        for(int i = 0;i < n; i++) ancestor.put(i, new HashSet<>());
-
-        
-        while(!queue.isEmpty()){
-            
-            int curr = queue.poll();
-            
-            for(int adj : graph.get(curr)){
-                ancestor.get(adj).add(curr);
-                ancestor.get(adj).addAll(ancestor.get(curr));
-                if(--indegree[adj] == 0) queue.add(adj);
-            }
-            
-        }
-        List<List<Integer>> ans = new ArrayList<>();
-        for(int i = 0; i < n; i++){
-            List<Integer> curr = new ArrayList<>(ancestor.get(i));
-            Collections.sort(curr);
-            ans.add(curr);
-        }
+        for(int i = 0; i < n; i++)
+            dfs(i,i,ans,graph);
         
         return ans;
         
     }
+    
+    private void dfs(int x, int curr, List<List<Integer>> ans, Map<Integer,List<Integer>> graph) {
+    for (int ch: graph.get(curr))
+        if(ans.get(ch).size() == 0 || ans.get(ch).get(ans.get(ch).size() - 1) != x) {
+            ans.get(ch).add(x);
+            dfs(x, ch, ans, graph);
+        }
+}
 }
