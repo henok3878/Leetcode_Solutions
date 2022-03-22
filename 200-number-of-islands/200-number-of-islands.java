5b4405f77a8dbca1db71c649413a5ec5e0f6fd46
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-23 00:40:57 +0300

    Time: 4 ms (69.43%), Space: 58 MB (17.27%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,33 +1,26 @@
 class Solution {
-    
-    int[][] directions = {{-1,0},{0,1},{1,0},{0,-1}};
-    
+    int[][] directions = {{1,0},{-1,0},{0,1},{0,-1}};
     public int numIslands(char[][] grid) {
-        int islands  = 0;
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == '1'){
-                    islands++;
-                    dfs(i,j,grid);
-                }
+        int m = grid.length, n = grid[0].length;
+        
+        int islands = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0;j < n; j++){
+                if(grid[i][j] == '0') continue;
+                dfs(i,j,grid);
+                islands++;
             }
         }
-        
         return islands;
     }
     
     private void dfs(int i, int j, char[][] grid){
-        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') return;
-        grid[i][j] = '*';
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') return;
+        grid[i][j] = '0';
         for(int[] dir : directions){
-            dfs(i + dir[0],j + dir[1],grid);
+            int x = i + dir[0], y = j + dir[1];
+            dfs(x,y,grid);
         }
     }
+    
 }
-
-/*
-st: 5:28 
-
-sub: 
-
-*/
