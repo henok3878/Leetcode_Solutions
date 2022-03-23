6152699c25e4fc22bc3f76a2aca8ca27f114f89d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-23 18:14:24 +0300

    Time: 47 ms (81.68%), Space: 72.8 MB (75.67%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,36 +1,35 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        
-        Set<String> pos = new HashSet<>(Arrays.asList(supplies));
-        Set<String> impos = new HashSet<>();
-        Map<String,Set<String>> graph = new HashMap<>();
-        for(int i = 0; i < recipes.length; i++)
-            graph.put(recipes[i],new HashSet<>(ingredients.get(i)));
-        
+     
+        Set<String> supp = new HashSet<String>(Arrays.asList(supplies));
+        Map<String,Boolean> isPossible = new HashMap<>();
         List<String> ans = new ArrayList<>();
+        Map<String,List<String>> graph = new HashMap<>();
         for(int i = 0; i < recipes.length; i++){
-            String recipe = recipes[i];
-            if(pos.contains(recipe)) ans.add(recipe);
-            else if(!impos.contains(recipe) && isPossible(recipe,pos,impos,graph))                    ans.add(recipe);
+            graph.put(recipes[i],ingredients.get(i));
+        }
+        for(int i = 0; i < recipes.length; i++){
+                if(isRecipePossible(recipes[i],isPossible,graph,supp))
+                    ans.add(recipes[i]);
         }
         
         return ans;
     }
     
-    private boolean isPossible(String recipe, Set<String> pos, Set<String> impos, Map<String,Set<String>> graph){
-        if(pos.contains(recipe)) return true;
-        else if(!graph.containsKey(recipe) || impos.contains(recipe)) return false;
-        impos.add(recipe);
-        for(String adj : graph.get(recipe)){
-           if(!isPossible(adj,pos,impos,graph)){
-                impos.add(adj);
+    private boolean isRecipePossible(String recipe, Map<String,Boolean> isPossible,Map<String,List<String>> ingredients, Set<String> supplies){
+        if(supplies.contains(recipe)) return true;
+        else if(isPossible.containsKey(recipe)) return isPossible.get(recipe);
+        else if(!ingredients.containsKey(recipe)) return false;
+        List<String> ings = ingredients.get(recipe);
+        isPossible.put(recipe,false);
+        for(String ing : ings){
+            if(!isRecipePossible(ing,isPossible,ingredients,supplies)){
+                isPossible.put(ing,false);
                 return false;
             }
         }
-        impos.remove(recipe);
-        pos.add(recipe);
+        
+        isPossible.put(recipe,true);
         return true;
     }
 }
-
-
