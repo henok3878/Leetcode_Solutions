9f2a0fb07bcb54d49d332b5819007c8e32f41023
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-23 18:07:54 +0300

    Time: 92 ms (49.98%), Space: 94.4 MB (22.44%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -11,13 +11,7 @@ class Solution {
         for(int i = 0; i < recipes.length; i++){
             String recipe = recipes[i];
             if(pos.contains(recipe)) ans.add(recipe);
-            else if(!impos.contains(recipe)){
-                    if(isPossible(recipe,pos,impos,graph)){
-                        ans.add(recipe);
-                        pos.add(recipe);
-                    }
-                    else impos.add(recipe);
-            }
+            else if(!impos.contains(recipe) && isPossible(recipe,pos,impos,graph))                    ans.add(recipe);
         }
         
         return ans;
@@ -28,15 +22,8 @@ class Solution {
         else if(!graph.containsKey(recipe) || impos.contains(recipe)) return false;
         impos.add(recipe);
         for(String adj : graph.get(recipe)){
-            if(graph.getOrDefault(adj,new HashSet<>()).contains(recipe)){
-                // cycle 
+           if(!isPossible(adj,pos,impos,graph)){
                 impos.add(adj);
-                impos.add(recipe);
-                return false;
-            }
-            else if(!isPossible(adj,pos,impos,graph)){
-                impos.add(adj);
-                impos.add(recipe);
                 return false;
             }
         }
@@ -46,28 +33,4 @@ class Solution {
     }
 }
 
-/*
-is not possible if, 1) there is a dependency cycle 
-
-                        Happens if my ingredients contains me.
-                        
-                    2) ingredient is not found in ingredients list 
-                    
-           ["p","we","bxgnm","vpio","i","hjvu","igi","anp","tokfq","z","kwdmb","g","qb","q",hthy"]
-           
-[
----
----
-["fzr","wi","otr","xgp","wbjr","igi","b"],["fzr","xgp","wi","otr","tokfq","izcad","igi","xevvq","i","anp"],["wi","xgp","wbjr"],["wbjr","bxgnm","i","b","hjvu","izcad","igi","z","g"],["xgp","otr","wbjr"],["wbjr","otr"],["wbjr","otr","fzr","wi","xgp","hjvu","tokfq","z","kwdmb"],["xgp","wi","wbjr","bxgnm","izcad","p","xevvq"],["bxgnm"],["wi","fzr","otr","wbjr"], ---
-["wbjr","wi","fzr","xgp","otr","g","b","p"],
-["otr","fzr","xgp","wbjr"],---
-["xgp","wbjr","q","vpio","tokfq","we"],
-["wbjr","wi","xgp","we"], 
-["wbjr"],---
-["wi"]], ---
-
-["wi","otr","wbjr","fzr","xgp","xevvq","g","izcad","b"]  
-
-o:p = [0]
-*/
 
