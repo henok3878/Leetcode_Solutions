258677eeb5ed910978fa8a2c7d463f4ba81ed3ce
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-23 22:33:06 +0300

    Time: 9 ms (58.42%), Space: 49.7 MB (39.70%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -4,27 +4,33 @@ class Solution {
         Map<Integer,List<Integer>> graph = new HashMap<>();
         for(int i = 0; i < numCourses; i++) graph.put(i,new ArrayList<>());
         
-        int[] indegree = new int[numCourses];
-        
         for(int[] pair : prerequisites){
             graph.get(pair[1]).add(pair[0]);
-            indegree[pair[0]]++;
         }
+        Boolean[] visited = new Boolean[numCourses];
         
-        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        
         for(int i = 0; i < numCourses; i++)
-            if(indegree[i] == 0) queue.add(i);
-        int visited = 0;
-        int[] ans = new int[numCourses];
+            if(!dfs(i,visited,ans,graph)) 
+                return new int[]{};
         
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            ans[visited++] = curr;
-            for(int adj : graph.get(curr))
-                if(--indegree[adj] == 0) queue.add(adj);
+        return ans.stream()
+                    .map(i -> (i == null ? 0 : i))
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+        
+    }
+    
+    private boolean dfs(int i,Boolean[] visited, List<Integer> ans, Map<Integer,List<Integer>> graph){
+        if(visited[i] != null) return visited[i];
+        
+        visited[i] = false;
+        for(int adj : graph.get(i)){
+            if(!dfs(adj,visited,ans,graph)) return false;
         }
-        
-        return (visited == numCourses) ? ans : new int[]{};
+        ans.add(0,i);
+        return visited[i] = true;
         
     }
 }
