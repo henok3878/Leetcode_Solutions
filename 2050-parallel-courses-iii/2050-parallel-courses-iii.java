efc47e8bd444ea61aad7d8fbd396907e067fb4ac
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-24 12:07:25 +0300

    Time: 48 ms (61.65%), Space: 129.8 MB (28.91%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.java
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.java
@@ -1,38 +1,38 @@
 class Solution {
-    
     public int minimumTime(int n, int[][] relations, int[] time) {
-        int[] timeNeeded = new int[n + 1];
+        
+        boolean[] visited = new boolean[n+1];
+        int[] timeNeeded = new int[n+1];
+        
         Map<Integer,List<Integer>> graph = new HashMap<>();
         
-        for(int i = 1; i <= n; i++){
-            timeNeeded[i] = time[i-1];
+        for(int i = 1;i <= n;i++){
             graph.put(i,new ArrayList<>());
         }
         
-        int[] indegree = new int[n+1];
-            
-        for(int[] pair : relations){
-            graph.get(pair[0]).add(pair[1]);
-            indegree[pair[1]]++;
+        for(int[] relation : relations){
+            graph.get(relation[1]).add(relation[0]);
         }
         
-        Queue<Integer> queue = new LinkedList<>();
-        
-        for(int i = 1; i <= n; i++) if(indegree[i] == 0) queue.add(i);
-        
         int ans = Integer.MIN_VALUE;
-        
-        while(!queue.isEmpty()){
-            int curr = queue.poll();
-            ans = Math.max(timeNeeded[curr],ans);
+        for(int i = 1; i<= n; i++){
             
-            for(int adj: graph.get(curr)){
-                if(--indegree[adj] == 0) 
-                    queue.add(adj);
-                timeNeeded[adj] = Math.max(time[adj-1] + timeNeeded[curr], timeNeeded[adj]);
-            }
+            ans = Math.max(ans,dfs(i,visited,timeNeeded,time,graph));
         }
         
         return ans;
+    
+    }
+    
+    private int dfs(int i,boolean[] visited,int[] timeNeeded,int[] time, Map<Integer,List<Integer>> graph){
+        if(visited[i]) return timeNeeded[i];
+        else if(graph.get(i).size() == 0) return time[i-1];
+        visited[i] = true;
+        int max = Integer.MIN_VALUE;
+        for(int adj : graph.get(i)){
+            max = Math.max(max,dfs(adj,visited,timeNeeded,time,graph));
+        }
+        
+        return timeNeeded[i] = time[i - 1] + max;
     }
 }
