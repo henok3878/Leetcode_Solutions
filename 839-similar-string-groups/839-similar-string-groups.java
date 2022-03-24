48b9362a2f849cf09216d7843d11b44020c72a4b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-25 00:40:29 +0300

    Time: 18 ms (70.71%), Space: 43.3 MB (71.36%) - LeetHub

--- 839-similar-string-groups/839-similar-string-groups.java
+++ 839-similar-string-groups/839-similar-string-groups.java
@@ -1,27 +1,17 @@
 class Solution {
     public int numSimilarGroups(String[] strs) {
-                     
-        Set<String> visited = new HashSet<>();
-        int groups = 0;
-        for(String s : strs){
-            if(!visited.contains(s)){
-                dfs(s,visited,strs);
-                groups++;
+        int n = strs.length;
+        int components = n;
+        UnionFind uf = new UnionFind(n);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = i + 1;j < n; j++){
+                if(isSimilar(strs[i],strs[j]))
+                    components -= uf.union(i,j);
             }
         }
         
-        return groups;
-        
-        
-    }
-    
-    private void dfs(String s, Set<String> visited, String[] strings){
-        if(visited.contains(s)) return;
-        visited.add(s);
-        for(String adj : strings){
-            if(isSimilar(s,adj))
-                dfs(adj,visited,strings);
-        }
+        return components;
     }
     
     
@@ -31,5 +21,39 @@ class Solution {
             if(s1.charAt(i) != s2.charAt(i) && ++count > 2) return false;
         }
         return true;
+    }
+}
+
+
+class UnionFind{
+    int[] parents;
+    int[] ranks;
+    
+    public UnionFind(int size){
+        parents = new int[size];
+        ranks = new int[size];
+        for(int i = 0; i < size; i++) parents[i] = i;
+    }
+    
+    public int find(int elem){
+        if(parents[elem] == elem) return elem;
+        return parents[elem] = find(parents[elem]);
+    }
+    
+    public int union(int elem1, int elem2){
+        int parent1 = find(elem1);
+        int parent2 = find(elem2);
+        
+        if(parent1 == parent2) return 0;
+        
+        if(ranks[parent1] > ranks[parent2])
+            parents[parent2] = parent1;
+        else if(ranks[parent1] < ranks[parent2])
+            parents[parent1] = parent2;
+        else{
+            parents[parent2] = parent1;
+            ranks[parent1]++;
+        }
+        return 1;
     }
 }
