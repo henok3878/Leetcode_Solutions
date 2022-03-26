bf9e02753c4dd286cbf70d71a6ef59fe5c435e35
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-26 12:55:36 +0300

    Time: 8 ms (96.87%), Space: 46.2 MB (88.78%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -11,19 +11,16 @@ class Solution {
         int[] distance = new int[n+1];
         Arrays.fill(distance,Integer.MAX_VALUE);
         distance[k] = 0;
-        boolean[] visited = new boolean[n+1];
+        
         Queue<int[]> queue = new PriorityQueue<>((a,b)->a[1] - b[1]);
         queue.add(new int[]{k,0});
         while(!queue.isEmpty()){
             int[] curr = queue.poll();
-            visited[curr[0]] = true;
             for(int[] adj : graph.get(curr[0])){
-                if(!visited[adj[0]]){
-                    int currDist = curr[1] + adj[1];
-                    if(distance[adj[0]] > currDist){
-                        distance[adj[0]] = currDist;
-                        queue.add(new int[]{adj[0],currDist});
-                    }
+                int currDist = curr[1] + adj[1];
+                if(distance[adj[0]] > currDist){
+                    distance[adj[0]] = currDist;
+                    queue.add(new int[]{adj[0],currDist});
                 }
             }
         }
