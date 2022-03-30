bafd24f6f0a0bf5d5d928a2342f85d00e9d6183e
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-03-31 00:21:53 +0300

    Time: 49 ms (14.21%), Space: 54.1 MB (11.58%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minDays(int n) {
+        
+        Set<Integer> visited = new HashSet<>();
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(n);
+        visited.add(n);
+        int min = 0;
+        
+        while(!q.isEmpty()){
+            int s = q.size();
+            for(int i = 0; i < s ;i++){
+                int curr = q.poll();
+                if(curr == 0) return min;
+                if(curr % 2 == 0 && !visited.contains(curr/2)){
+                    visited.add(curr/2);
+                    q.add(curr/2);
+                }
+                if(curr % 3 == 0 && !visited.contains(curr/3)){
+                    visited.add(curr/3);
+                    q.add(curr/3);
+                }
+                if(!visited.contains(curr - 1))
+                {q.add(curr-1); visited.add(curr-1);}
+            }
+            min++;
+            
+        }
+        return min;
+    }
+}
