26ba6940de5ea32fd68d7d207fa689ac53192575
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-01 23:28:51 +0300

    Time: 7 ms (93.14%), Space: 74 MB (74.51%) - LeetHub

---
+++ 1562-find-latest-group-of-size-m/1562-find-latest-group-of-size-m.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int findLatestStep(int[] arr, int m) {
+        int n = arr.length;
+        int[] len = new int[n+1];
+        
+        int count = 0;
+        int latest = -1;
+        for(int i = 0;i < n; i++){
+            int curr = arr[i], left = (arr[i] - 1 >= 0) ? len[arr[i] - 1] : 0, right = (arr[i] + 1 <= n) ? len[arr[i] + 1] : 0;
+            if(left == m) count--;
+            if(right == m) count--;
+            len[curr] = len[curr-left] = len[curr + right] =  left + right + 1;
+            
+            if(len[curr] == m) count++;
+            
+            if(count > 0) latest = i + 1;
+            
+        }
+        
+        return latest;
+    }
+}
+
+/*
+
+
+--84248328
+
+-$***$--^**^--
+
+
+*/
