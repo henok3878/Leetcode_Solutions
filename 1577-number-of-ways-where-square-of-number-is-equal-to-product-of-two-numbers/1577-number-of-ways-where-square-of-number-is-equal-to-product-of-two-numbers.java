0cc2c295a10b47579edc841cdff6b927d0c11492
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-03 21:40:22 +0300

    Time: 249 ms (46.73%), Space: 41.7 MB (96.26%) - LeetHub

---
+++ 1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers/1577-number-of-ways-where-square-of-number-is-equal-to-product-of-two-numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numTriplets(int[] nums1, int[] nums2) {
+        
+        int count = 0;
+        
+        for(int i = 0; i < nums1.length; i++)
+            count+= helper(nums2,((long)nums1[i] * nums1[i]));
+            
+        for(int i = 0; i < nums2.length; i++)
+           count += helper(nums1,(long)Math.pow(nums2[i],2)); 
+        
+        return count;
+    }
+    
+    private int helper(int[] nums,long sq){
+        int count = 0;
+        Map<Integer,Integer> visited = new HashMap<>();
+        for(int j = 0;j < nums.length; j++){
+            int curr = nums[j];
+            if(sq % curr == 0.0){
+                int comp = (int)(sq / curr);
+                if(visited.containsKey(comp)) count += visited.get(comp);
+
+            }
+            int prev = visited.getOrDefault(curr,0) + 1;
+            visited.put(curr,prev);
+        }
+        return count;
+    }
+}
