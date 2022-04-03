a83f65947616ad9fa76ec2d79a9a058594eff807
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-03 23:17:39 +0300

    Time: 2 ms (22.28%), Space: 44.2 MB (10.62%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -2,32 +2,52 @@ class Solution {
     public void nextPermutation(int[] nums) {
         
         int n = nums.length;
+        int idx = -1;
         
-        boolean found = false;
-        for(int i = n-1; i >= 0; i--){
-            int minIdx = -1;
-            int min = Integer.MAX_VALUE;
-            
-            for(int j = i; j < n; j++){
-                if(nums[j] > nums[i] && min > nums[j]){
-                    minIdx = j;
-                    min = nums[j];
-                    found = true;
-                }
-            }
-            if(found){
-                nums[minIdx]  = nums[i];
-                nums[i] = min;
-                Arrays.sort(nums,i+1,n);
+        for(int i = n-1; i > 0; i--){ 
+            if(nums[i-1] < nums[i]){
+                idx = i-1;
                 break;
             }
-        }        
-        if(!found) Arrays.sort(nums);
-       
+        }
+        if(idx == -1)  reverse(nums,0);     
+        else{
+            int minIdx = -1;
+            int min = Integer.MAX_VALUE;
+
+            for(int j = idx + 1; j < n;j++){
+                if(nums[j] > nums[idx] && min >= nums[j]){
+                    minIdx = j;
+                    min = nums[j];
+                }
+            }
+            if(idx != -1 && minIdx != -1){
+                swap(nums,idx,minIdx);
+            }
+            reverse(nums,idx+1);    
+        }
+    }
+    
+    private void reverse(int[] nums, int idx){
+        int n = nums.length;
+        int left = idx, right = n-1;
+        while(left <= right){
+            swap(nums,left,right);
+            left++;
+            right--;
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
     }
 }
 
 /*
+
+[2,3,1,3,3]
 
 [1,2,3] => 123 132 213 231 312 321 123 (cycle)
 
