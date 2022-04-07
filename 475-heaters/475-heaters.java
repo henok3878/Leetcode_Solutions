9664dc643d2b17766c3d3ce2b19f77a587d67d0d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-07 22:50:42 +0300

    Time: 37 ms (22.28%), Space: 54.9 MB (18.74%) - LeetHub

---
+++ 475-heaters/475-heaters.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int htrs = heaters.length;
+        int ans = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < houses.length; i++){
+            int near = find(0,houses[i],heaters);
+            //System.out.println("h: " + houses[i]  + " , near: " + near);
+            int curr = -1;
+            if(near < 0){
+                curr = heaters[0] - houses[i];
+            }else if(near >= htrs){
+                curr = houses[i] - heaters[htrs-1];
+            }
+            else{
+                int left = (near - 1 >= 0) ? Math.abs(houses[i] - heaters[near-1]) : Integer.MAX_VALUE;
+                int right = Math.abs(houses[i] - heaters[near]);
+                curr = Math.min(left,right);
+            }
+            ans = Math.max(curr,ans);
+        }
+        return ans;
+        
+    }
+    
+    private int find(int i, int val, int[] hs){
+        int st = i, end = hs.length-1;
+        while(st <= end){
+            int mid = st + (end - st) /2 ;
+            if(hs[mid] < val){
+                st = mid + 1;
+            }
+            else end = mid - 1;
+        }
+        return st;
+    }
+}
