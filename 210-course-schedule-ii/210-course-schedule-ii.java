048c3b61c78c17121a43acd36405de1dbfecf44b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-09 19:08:16 +0300

    Time: 3 ms (99.02%), Space: 43.3 MB (88.03%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -1,36 +1,37 @@
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++) graph.add(new ArrayList<>());
+        int[] ans = new int[numCourses];
+        int idx = 0;
         
-        for(int[] pair : prerequisites){
-            int u = pair[0], v = pair[1];
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) 
+            graph.add(new ArrayList<>());
+        
+        int[] indegree = new int[numCourses];
+        
+        for(int[] pre : prerequisites){
+            int u = pre[0] , v = pre[1];
             graph.get(v).add(u);
+            indegree[u]++;
         }
         
-        Boolean[] visited = new Boolean[numCourses];
-        List<Integer> ans = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++) 
-            if(!dfs(i,visited,ans,graph))
-                return new int[]{};
-        int[] res = new int[numCourses];
-        for(int i = 0; i < ans.size(); i++) res[i] = ans.get(i);
-        return res;
+        Queue<Integer> q = new LinkedList<>();
         
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0)
+                q.add(i);
+        }
+        
+        while(!q.isEmpty()){
+            int curr = q.poll();
+            ans[idx++] = curr;
+            for(int adj: graph.get(curr)){
+                if(--indegree[adj] == 0){
+                    q.add(adj);
+                }
+            }
+        }
+        
+        return (idx == numCourses) ? ans : new int[0];
     }
-    
-    private boolean dfs(int curr, Boolean[] visited, List<Integer> ans, List<List<Integer>> graph){
-        if(visited[curr] != null) return visited[curr];
-        
-        visited[curr] = false;
-        
-        for(int adj : graph.get(curr))
-            if(!dfs(adj,visited,ans,graph))
-                return false;
-        
-        ans.add(0,curr);
-        return visited[curr] = true;
-            
-    }
-    
 }
