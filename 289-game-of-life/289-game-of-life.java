c67e017872977c03923396300b23893583652b8f
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-12 10:36:52 +0300

    Time: 1 ms (55.24%), Space: 41.9 MB (76.21%) - LeetHub

--- 289-game-of-life/289-game-of-life.java
+++ 289-game-of-life/289-game-of-life.java
@@ -4,38 +4,36 @@ class Solution {
     public void gameOfLife(int[][] board) {
         int m = board.length;
         int n = board[0].length;
-        int[][] count = new int[m][n];
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
+                int cnt = 0, state = board[i][j];
                 for(int[] adj : adjs){
                     int x = i + adj[0], y = j + adj[1];
-                    if(isInBound(x,y,m,n) && board[x][y] == 1){
-                        count[i][j]++;
+                    if(isInBound(x,y,m,n) && board[x][y]%2 == 1){
+                       cnt++;
                     }
-                }   
-            }
-        }
-        // for(int[] r : count)
-        //     System.out.println(Arrays.toString(r));
-        
-        for(int i = 0; i < m; i++){
-            for(int j = 0;j  < n; j++){
-                int cnt = count[i][j];
-                int state = board[i][j];
+                }
                 if(state == 0 && cnt == 3){
-                    board[i][j] = 1;
+                    board[i][j] = 2;
                 }
                 else if(state == 1 && cnt > 3){
-                    board[i][j] = 0;
+                    board[i][j] = 3;
                 }
                 else if(state == 1 && cnt < 2){
-                    board[i][j] = 0;
-                }else if(state == 1 && (cnt == 3 || cnt <= 2)){
-                    board[i][j] = 1;
+                    board[i][j] = 3;
                 }
+                
             }
         }
+        
+        for(int i = 0; i < m;i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 2) board[i][j] = 1;
+                else if(board[i][j] == 3) board[i][j] = 0;
+            }
+        }
+     
     }
     
     private boolean isInBound(int i, int j, int m, int n){
@@ -44,11 +42,3 @@ class Solution {
         return true;
     }
 }
-
-/*
-live -> < 2 live -> die
-live -> 2 >= x <= 3 live -> live
-
-
-
-*/
