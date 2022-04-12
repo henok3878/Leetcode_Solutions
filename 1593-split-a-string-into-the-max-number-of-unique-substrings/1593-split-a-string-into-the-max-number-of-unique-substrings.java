10900c809f7cf662ba29eafadbe646aa2ff14640
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-13 00:47:02 +0300

    Time: 43 ms (75.71%), Space: 42.5 MB (87.74%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.java
@@ -1,38 +1,35 @@
 class Solution {
     public int maxUniqueSplit(String s) {
-        Set<String> set = new HashSet<>();
-        return helper(s,0,set);
+        return helper(0,s,new HashSet<>());
     }
     
-    private int helper(String s, int st, Set<String> visited){
-        //System.out.println(s.substring(st));
-        if(st >= s.length())
+    private int helper(int idx,String s, Set<String> v){
+        if(idx >= s.length())
             return 0;
-        int res = 0;
-        for(int end = st + 1; end <= s.length(); end++){
-            //choose 
-            String str = s.substring(st,end);
-            if(visited.contains(str))
-                continue;
-            visited.add(str);
-            // explore 
-            res = Math.max(res,1 + helper(s,end,visited));
-            visited.remove(str);
-            // unchoose
+        
+        int res = Integer.MIN_VALUE;
+        // for loop simulate decision spaces
+        for(int end = idx + 1; end <= s.length(); end++){
+            
+            String sub = s.substring(idx,end);
+            
+            // constraint 
+            if(!v.contains(sub)){
+                v.add(sub);
+                res = Math.max(res, 1 + helper(end,s,v));
+                v.remove(sub);
+            }
         }
-       
+        
         return res;
     }
-    
 }
 
 /*
-Input: s = "a b a b c c c"
-            0 1 2 3 4 5 6
-Output: 5
-Explanation: One way to split maximally is ['a', 'b', 'ab', 'c', 'cc']. Splitting like ['a', 'b', 'a', 'b', 'c', 'cc'] is not valid as you have 'a' and 'b' multiple times.
+goal: reach the end while satsfiying constraints 
+constriant: non-empty, unique 
 
-a,babcc
-ab,abcc
+state: index
+
 
 */
