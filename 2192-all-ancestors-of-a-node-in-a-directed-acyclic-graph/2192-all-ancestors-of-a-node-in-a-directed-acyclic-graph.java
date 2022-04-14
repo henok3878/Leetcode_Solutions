a06bd022eb44733f41edc6f36777ce909731ebaf
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-14 06:00:54 +0300

    Time: 73 ms (95.08%), Space: 66.2 MB (92.67%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,29 +1,30 @@
 class Solution {
     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        Map<Integer,List<Integer>> graph = new HashMap<>();
+        List<List<Integer>> graph = new ArrayList<>();
         List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i = 0;i < n; i++) {
+        for(int i = 0;i < n; i++){
+            graph.add(new ArrayList<>());
             ans.add(new ArrayList<>());
-            graph.put(i, new ArrayList<>());
         }
-        
-        for(int[] edge : edges){
+        for(int[] edge : edges)
             graph.get(edge[0]).add(edge[1]);
-        }
         
-        for(int i = 0; i < n; i++)
-            dfs(i,i,ans,graph);
+        for(int i = 0; i < n; i++){
+            dfs(i,i,graph,ans);
+        }
         
         return ans;
-        
     }
     
-    private void dfs(int x, int curr, List<List<Integer>> ans, Map<Integer,List<Integer>> graph) {
-    for (int ch: graph.get(curr))
-        if(ans.get(ch).size() == 0 || ans.get(ch).get(ans.get(ch).size() - 1) != x) {
-            ans.get(ch).add(x);
-            dfs(x, ch, ans, graph);
+    private void dfs(int p, int n, List<List<Integer>> graph, List<List<Integer>> ans){
+        
+        for(int adj : graph.get(n)){
+            int size = ans.get(adj).size();
+            if(size == 0 || ans.get(adj).get(size - 1) != p){
+                ans.get(adj).add(p);
+                dfs(p,adj,graph,ans);
+            }
         }
-}
+    }
 }
