ac70d3dce0e416dd5078f6c884c6ddc8378f84a2
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-15 12:41:38 +0300

    Time: 518 ms (42.28%), Space: 68.1 MB (82.21%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -1,62 +1,64 @@
 class Solution {
     public int minCostConnectPoints(int[][] points) {
-        int n = points.length;
-        
-        List<int[]>edges = new ArrayList<>();
-        
-        for(int i = 0;i < n; i++){
+        List<int[]> edges = new ArrayList<>();
+        int pts = points.length;
+        for(int i = 0;i < pts; i++){
             int[] p1 = points[i];
-            for(int j = i + 1; j < n; j++){
+            for(int j = i + 1; j < pts; j++){
                 int[] p2 = points[j];
-                edges.add(new int[]{i,j,Math.abs(p2[0] - p1[0]) + Math.abs(p2[1] - p1[1])});
+                int dist = Math.abs(p2[0] - p1[0]) + Math.abs(p2[1] - p1[1]);
+                edges.add(new int[]{i,j,dist});
             }
         }
+        UnionFind uf = new UnionFind(pts);
         
-        Collections.sort(edges,(a,b)->a[2] - b[2]);
-        
-        UnionFind uf = new UnionFind(n);
+        Collections.sort(edges,(a,b)-> a[2] - b[2]);
         int cost = 0;
-        
-        for(int[] e : edges){
-             int i = e[0], j = e[1];
-             if(uf.union(i,j)) cost += e[2];
+        for(int[] edge : edges){
+            if(uf.union(edge[0],edge[1]))
+                cost += edge[2];
         }
         
         return cost;
     }
 }
-
 class UnionFind{
     
     int[] parents;
     int[] ranks;
-    
     public UnionFind(int n){
         parents = new int[n];
         ranks = new int[n];
-        for(int i = 0; i < n; i++) parents[i] = i;
+        for(int i = 0; i < n; i++)
+            parents[i] = i;
     }
     
-    public int find(int elem){
-        if(parents[elem] == elem) return elem;
-        return parents[elem] = find(parents[elem]);
+    public int find(int el){
+        if(parents[el] == el) 
+            return el;
+        return parents[el] = find(parents[el]);
     }
     
-    public boolean union(int elem1, int elem2){
-        int p1 = find(elem1);
-        int p2 = find(elem2);
-        
-        if(p1 == p2) return false;
-        
-        if(ranks[p1] > ranks[p2]) parents[p2] = p1;
-        else if(ranks[p1] < ranks[p2]) parents[p1] = p2;
+    public boolean union(int el1, int el2){
+        int p1 = find(el1);
+        int p2 = find(el2);
+        if(p1 == p2) 
+            return false;
+        if(ranks[p1] > ranks[p2])
+            parents[p2] = p1;
+        else if(ranks[p1] < ranks[p2])
+            parents[p1] = p2;
         else{
             parents[p2] = p1;
-            ranks[p1]++;
+            ranks[p2]++;
         }
         
         return true;
     }
-    
-    
 }
+
+/*
+    Minimum spanning tree 
+
+
+*/
