9f8ed3e95d765088d6ea86b23ef798ab1f572e1e
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-15 07:17:34 +0300

    Time: 1 ms (94.98%), Space: 43.4 MB (81.60%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,28 +1,59 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        
-        boolean[] visited = new boolean[isConnected.length];
-            
-        int ans = 0;
-        
-        for(int i = 0; i < isConnected.length; i++){
-            // for each cell, call dfs 
-            if(!visited[i]){
-                ans++;
-                dfs(i,visited,isConnected);
-            }
-        }
+        int n = isConnected.length;
+        UnionFind uf = new UnionFind(n);
+    
+       int ans = n;
+       for(int i = 0; i < n; i++){
+           for(int j = 0; j < n; j++){
+               if(isConnected[i][j] == 1){
+                   ans -= uf.union(i,j);
+               }
+           }
+       }
         
         return ans;
-    }
-    
-    private void dfs(int i, boolean[] visited, int[][] graph){
-        if(visited[i]) return;
-        visited[i] = true;
-        for(int city = 0; city < graph.length; city++){
-            if(graph[i][city] == 1 ){
-                dfs(city,visited,graph);
-            }
-        }
+        
     }
 }
+
+class UnionFind{
+    
+    int[] parents;
+    int[] ranks;
+    
+    public UnionFind(int n){
+        parents = new int[n];
+        ranks = new int[n];
+        for(int i = 0;i < n; i++){
+            parents[i] = i;
+        }
+    }
+    
+    public int find(int el){
+        if(parents[el] == el) return el;
+        return parents[el] = find(parents[el]);
+    }
+    
+    public int union(int el1,int el2){
+        int p1 = find(el1);
+        int p2 = find(el2);
+        if(p1 == p2)
+            return 0;
+        if(ranks[p1] > ranks[p2])
+            parents[p2] = p1;
+        else if(ranks[p2] > ranks[p1])
+            parents[p1] = p2;
+        else{
+            parents[p2] = p1;
+            ranks[p1]++;
+        }
+        
+        return 1;
+    }
+}
+/*
+
+
+
+*/
