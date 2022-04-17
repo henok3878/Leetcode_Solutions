78b32f29a25a4caa22995ed6127f9c95a41d7cd8
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-17 22:00:46 +0300

    Time: 67 ms (84.05%), Space: 15.2 MB (25.72%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i,b in enumerate(nums):
+            a = target - b
+            if a in seen:
+                return [seen[a],i]
+            seen[b] = i
