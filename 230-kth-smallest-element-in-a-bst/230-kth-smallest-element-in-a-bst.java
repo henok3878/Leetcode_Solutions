8d8b896d92a63b64dc4eba8cdd86ec4d0a45cb49
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-18 05:43:53 +0300

    Time: 0 ms (100.00%), Space: 42.1 MB (91.65%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -1,18 +1,34 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    
-    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        list = []
-        def inOrder(root):
-            if root is None: return
-            inOrder(root.left)
-            list.append(root.val)
-            inOrder(root.right)
-            
-        inOrder(root)
-        return list[k-1]
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int number = -1;
+    int cnt  = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root,k);
+        return number;
+    }
+    private void dfs(TreeNode root,int k){
+        if(root.left != null)
+            dfs(root.left,k);
+        cnt++;
+        if(k == cnt){
+            number = root.val;
+            return;
+        }
+        if(root.right != null)
+            dfs(root.right,k);
+    }
+}
