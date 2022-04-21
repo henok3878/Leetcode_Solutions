451aa57a314b271d94930970a273addc5ac775b1
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-21 12:32:57 +0300

    Time: 56 ms (13.38%), Space: 63.2 MB (54.32%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.java
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.java
@@ -3,12 +3,19 @@ class Solution {
         
         int size = 10;
         int n = s.length(); 
+        if(n <= size)
+            return new ArrayList<>();
         
         Set<String> visited = new HashSet<>();
         Set<String> ans = new HashSet<>();
         
+        StringBuilder sb = new StringBuilder(s.substring(0,size));
         for(int i = 0;i <= n - size; i++){
-            String str = s.substring(i, i + size);
+            if(i > 0){
+                sb.deleteCharAt(0);
+                sb.append(s.charAt(i + size - 1));
+            }
+            String str = sb.toString();
             if(visited.contains(str))
                 ans.add(str);
             else
