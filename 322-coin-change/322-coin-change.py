a74b0a64f2d38cc26f5c1c3a0868d2905284ed14
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-23 23:40:04 +0300

    Time: 2729 ms (16.11%), Space: 16.2 MB (24.69%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,11 +1,20 @@
 class Solution:
+    
     max = 10**6
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [self.max]*(amount + 1)
-        dp[0] = 0
-        for n in range(amount + 1):
-            for coin in coins:
+        N = len(coins)
+        dp = [[self.max for _ in range(amount + 1)] for _ in range(N + 1)]
+        
+        for i in range(N + 1):
+            dp[i][0] = 0
+        
+        for i in range (1,N + 1):
+            for n in range (1,amount + 1):
+                coin = coins[i-1]
+                dp[i][n] = dp[i-1][n]
                 if n >= coin:
-                    dp[n] = min(dp[n-coin] + 1, dp[n])
-                    
-        return -1 if dp[amount] == self.max else dp[amount] 
+                    dp[i][n] = min(dp[i][n], dp[i][n-coin] + 1)
+                                        
+        return -1 if dp[N][amount] == self.max else dp[N][amount]
+        
+        
