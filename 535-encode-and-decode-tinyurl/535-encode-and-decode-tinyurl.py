8c9083b92638881d20274ce627c09ba466d4bf9b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-23 09:55:10 +0300

    Time: 39 ms (74.32%), Space: 13.9 MB (69.91%) - LeetHub

---
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.py
@@ -0,0 +1,24 @@
+class Codec:
+    counter = 0
+    base = "https://tiny.com/"
+    def __init__(self):
+        self.map = {}
+    
+    def encode(self, longUrl: str) -> str:
+        """Encodes a URL to a shortened URL.
+        """
+        curr = self.base + str(self.counter)   
+        self.map[self.counter] = longUrl
+        self.counter += 1
+        return curr
+
+    def decode(self, shortUrl: str) -> str:
+        """Decodes a shortened URL to its original URL.
+        """
+        a = int(shortUrl.split('/')[-1])
+        return self.map[a]
+        
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
