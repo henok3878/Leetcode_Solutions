ea6ba1ad5fd5217f9a797ba917d2ba77be086f88
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-24 14:03:31 +0300

    Time: 451 ms (11.95%), Space: 25.6 MB (9.74%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,8 +1,10 @@
 class UndergroundSystem:
 
+    def fact(self):
+        return (0,0)
     def __init__(self):
         self.check_ins = {} # id -> [st,t]
-        self.trips = {} # st_end -> total, count
+        self.trips = defaultdict(self.fact)# st_end -> total, count
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
         self.check_ins[id] = [stationName,t]
@@ -10,7 +12,7 @@ class UndergroundSystem:
     def checkOut(self, id: int, stationName: str, t: int) -> None:
         st_station,st_time = self.check_ins[id]
         key = self.getKey(st_station,stationName)
-        total,count = self.trips.get(key,(0,0))
+        total,count = self.trips[key]
         self.trips[key] = (total + t - st_time, count + 1)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
