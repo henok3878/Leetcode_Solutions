62fab43bd81c7b438acf738477ba2a8cf2fb7fe5
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-26 10:34:54 +0300

    Time: 569 ms (33.84%), Space: 29.4 MB (38.08%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -2,11 +2,9 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         N = len(coins)
         dp = [[0]*(amount+1) for _ in range(N + 1)]
+        dp[0][0] = 1
         
-        for i in range(N+1):
-            dp[i][0] = 1
-        
-        for amt in range(1 , amount + 1):
+        for amt in range(amount + 1):
             for i in range(1, N + 1):
                 dp[i][amt] = dp[i-1][amt]
                 if amt - coins[i-1] >= 0:
