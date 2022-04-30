7927e20d8abdfe2058fa4632c120a3d8d47f4541
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-04-30 07:35:20 +0300

    Time: 46 ms (43.20%), Space: 13.9 MB (60.05%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.py
@@ -0,0 +1,34 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        n = len(equations)
+        for idx in range(n):
+            eq = equations[idx]
+            u,v,cost = eq[0], eq[1], values[idx]
+            graph[u].append((v,cost))
+            graph[v].append((u,1/cost))
+            
+       
+        def is_reachable(st,end,res):
+            if(len(graph[st]) == 0):
+                return -1
+            if(st == end):
+                return res
+            visited[st] = True
+            
+            for adj in graph[st]:
+                if(not visited[adj[0]]):
+                    temp = is_reachable(adj[0],end,adj[1] * res)
+                    if(temp != -1):
+                        return temp
+            return -1
+            
+        ql = len(queries)
+        ans = [-1]*ql
+        for i in range(ql):
+            q = queries[i]
+            u,v = q[0],q[1]
+            visited = defaultdict(bool)
+            ans[i] = is_reachable(u,v,1)
+    
+        return ans
