f23bc79a2277732a2e8c06a9f0b8721ba9246847
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-02 15:21:23 +0300

    Time: 2088 ms (41.20%), Space: 21.2 MB (16.66%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -5,21 +5,44 @@ class Solution:
         
         n = len(coins)
         m = 10**6
-        dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
-        for i in range(n):
-            dp[i][0] = 0
+        dp = [-1] * (amount + 1)
+        dp[0] = 0
         
-        def helper(i,amt):
-            if(i >= n or  amt < 0):
+        
+        def helper(amt):
+            if(amt < 0):
                 return m 
-            if(dp[i][amt] != -1):
-                return dp[i][amt]
+            if(dp[amt] != -1):
+                return dp[amt]
             
-            select = helper(i,amt - coins[i]) + 1
-            skip = helper(i+1,amt)
+            res = m
+            for c in coins:
+                res = min(res,helper(amt - c) + 1)
             
-            dp[i][amt] = min(skip,select)
-            return dp[i][amt]
-        # print(dp)
-        ret = helper(0,amount)
+            dp[amt] = res
+            return dp[amt]
+        ret = helper(amount)
         return -1 if ret == m else ret
+    
+
+"""
+
+    
+    -------------- n = 11
+   '
+   '
+   '
+    sum(nCk) for k in range(12): 
+    
+    = 2^n = 2^11
+    
+    1,1,1,1,1,1,1,1,2
+    
+    
+    {a,b,c} -> {},{a},{b},{b,c},{a,b},...,{a,b,c} # 8 
+    
+    {1,2,5} => 2^3 - - -
+    sum (nCk) for k in range(4)
+
+
+"""
