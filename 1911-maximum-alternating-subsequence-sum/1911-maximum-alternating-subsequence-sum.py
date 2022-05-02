76bf8c9339f037c5a84b21bacb2cfdba3751ffef
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-02 14:06:14 +0300

    Time: 1896 ms (49.88%), Space: 29 MB (54.34%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -2,11 +2,11 @@ class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         
         n = len(nums)
-        evens  = [0] * (n + 1)
-        odds = [0] * (n + 1)
+        even  = 0
+        odd = 0
         for i in range(1,n+1):
-         
-            evens[i] = max(evens[i-1],odds[i-1] - nums[i-1])
-            odds[i] = max(odds[i-1],evens[i-1] + nums[i-1])
+            temp = even
+            even = max(even,odd - nums[i-1])
+            odd = max(odd,even + nums[i-1])
             
-        return max(evens[n],odds[n])
+        return max(even,odd)
