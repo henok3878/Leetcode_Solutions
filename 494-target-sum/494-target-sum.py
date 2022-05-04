5c1bed3b07b59e1ebb7857f573323b13f5a5db0c
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-04 22:22:40 +0300

    Time: 352 ms (68.53%), Space: 41.8 MB (5.56%) - LeetHub

---
+++ 494-target-sum/494-target-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        n = len(nums)
+        
+        @lru_cache(None)
+        def rec(i,curr):
+            if(i  == n):
+                return 1 if curr == target else 0
+            
+            select = rec(i+1,curr - nums[i])
+            skip = rec(i+1,curr + nums[i])
+            
+            return select + skip;
+        
+        
+        return rec(0,0)
