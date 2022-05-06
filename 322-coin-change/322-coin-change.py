f68a74c3084dd9acf7d5910339e6e0f8768ca834
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-06 10:42:09 +0300

    Time: 2919 ms (12.49%), Space: 36.8 MB (8.50%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -2,26 +2,26 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         MAX = 10**6
-        
-        @lru_cache(None)
+        dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
+        dp[0][0] = 0
         
         def helper(i,amt):
             if i == n:
                 return 0 if amt == 0 else MAX
             elif amt < 0:
                 return MAX
-            
-            return  min(1 + helper(i,amt - coins[i]), helper(i+1,amt))             
+            elif dp[i][amt] == -1:
+                dp[i][amt] = min(1 + helper(i,amt - coins[i]), helper(i+1,amt))   
+                
+            return dp[i][amt]
         
         ans = helper(0,amount)
         
         return -1 if ans >= MAX else ans
     
     
-    """
-    [1,2,5], 11 
-    
-    select
-    skip
-    
-    """
+        """
+        - with out caching : 2^n
+        - with caching: n * amount 
+        So, we have avoided not to recompute 2^n - (n*amount) subproblems 
+        """
