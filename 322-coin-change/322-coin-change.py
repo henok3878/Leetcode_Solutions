b3715dffea6b52366ccd787621751dddec2d23e9
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-06 10:05:50 +0300

    Time: 1584 ms (73.92%), Space: 35.1 MB (10.07%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,41 +1,19 @@
 class Solution:
-    
-   
     def coinChange(self, coins: List[int], amount: int) -> int:
         
-        n = len(coins)
-        m = 10**6
-        dp = [-1] * (amount + 1)
-        dp[0] = 0
-        
-        
+        MAX = 10**6
+        @lru_cache(None)
         def helper(amt):
-            if(amt < 0):
-                return m 
-            if(dp[amt] != -1):
-                return dp[amt]
-            
-            res = m
+            if(amt == 0):
+                return 0
+            elif amt < 0:
+                return MAX 
+            res = MAX
             for c in coins:
-                res = min(res,helper(amt - c))
+                res = min(res,helper(amt - c) + 1)
             
-            dp[amt] = res + 1
-            return dp[amt]
-        ret = helper(amount)
-        return -1 if ret == (m + 1) else ret
-    
-
-"""
-
-    minWays(11) = 1 + Min(minWays(10), minWays(9), minWays(6))
-
-    
-    -------------- n = 11
-   '
-   '
-   '
-
-    sum (nCk) for k in range(4)
-
-
-"""
+            return res
+        
+        ans = helper(amount)
+        
+        return -1 if ans == MAX else ans
