e55fd8a38d5969689d7b8d8263cf2a735f2c3aca
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-07 14:50:27 +0300

    Time: 2389 ms (27.10%), Space: 35 MB (10.11%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,26 +1,60 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
         MAX = 10**6
-        dp = [[-1 for _ in range(amount + 1)] for _ in range(n)]
         
-        def helper(i,amt):
-            if i == n:
-                return 0 if amt == 0 else MAX
+        @lru_cache(None)
+        def helper(amt):
+            if amt == 0:
+                return 0
             elif amt < 0:
                 return MAX
-            elif dp[i][amt] == -1:
-                dp[i][amt] = min(1 + helper(i,amt - coins[i]), helper(i+1,amt))   
-                
-            return dp[i][amt]
+            res = MAX
+            for c in coins:
+                res = min(res,1 + helper(amt - c))
+            return res
         
-        ans = helper(0,amount)
+        ans = helper(amount)
         
-        return -1 if ans >= MAX else ans
-    
-    
+        return -1 if ans >= MAX else ans 
+        
+        
+        
         """
-        - with out caching : 2^n
-        - with caching: n * amount 
-        So, we have avoided not to recompute 2^n - (n*amount) subproblems 
+            
+        
+        The obvious answer: 
+            -> Generate every combination
+            -> Check if sum(combnination) == target 
+            -> track the min length
+            
+            TIME COMPLEXITY: 2^max_len , assuming we keep track of the sum while generating
+                -> max_len becuase we have infinte number of each kind of coin
+                
+               
+               
+              
+              
+        coins = [1,2,5], amount = 11
+        
+        if I used 1: 
+            amount = 10, min ways to make 10 using:
+                option1: using current and prev coins
+                    -> in this case you process amt only using available coins so far 
+                option2: using any possible coins you want 
+                    -> in this case you have to compute amount sequentially 
+                    
+                    
+                why both cases works:
+                    -> option 1 works because it covers every combination that can be formed
+                    -> option 2 works for the same reason but it also premituations of combinations. But if the problems was not optimization problem, this method will overcount combinations.
+                    
+                    amt called n times 
+                    total call => amount+1 * n unique problems 
+                    
+                    total = n^(amount + 1)
+            
+        
+        
+        
+        
         """
