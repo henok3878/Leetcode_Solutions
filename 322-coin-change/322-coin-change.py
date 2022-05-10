b4ae4f350c3a0540547bbdae7c8a4e6f8d0e96c9
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-10 09:55:33 +0300

    Time: 2037 ms (44.35%), Space: 14.2 MB (46.27%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,16 +3,15 @@ class Solution:
         
         MAX = 10**10
         n = len(coins)
-        dp = [[MAX for _ in range(amount + 1)] for _ in range(n + 1)]
-        dp[0][0] = 0
+        dp = [MAX for _ in range(amount + 1)]
+        dp[0] = 0
         
         for amt in range(amount + 1):
             for i in range(1,n + 1):
-                dp[i][amt] = dp[i-1][amt]
                 if amt - coins[i-1] >= 0:
-                    dp[i][amt] = min(dp[i][amt],1 + dp[i][amt -coins[i-1]])
+                    dp[amt] = min(dp[amt],1 + dp[amt -coins[i-1]])
                     
-        return -1 if dp[n][amount] >= MAX else dp[n][amount]
+        return -1 if dp[amount] >= MAX else dp[amount]
         
         
         """
