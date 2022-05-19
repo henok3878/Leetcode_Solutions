d083b7cfbbcacd90eaa12b3c8f055f92fb35821b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-19 22:23:00 +0300

    Time: 146 ms (20.75%), Space: 14.3 MB (98.79%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        MIN = -1*(10 ** 10)
+        neg, pos = (1,MIN)
+        n = len(nums)
+        ans = MIN
+        for i in range(n):
+            
+            if nums[i] <= 0:
+                t = pos 
+                pos = neg * nums[i]
+                neg = t * nums[i]
+                neg = min(neg,nums[i])
+            else:
+                pos *= nums[i]
+                neg *= nums[i]
+                pos = max(pos,nums[i])
+                
+            #print(pos,neg)
+            ans = max(pos,ans)
+            
+        return ans
