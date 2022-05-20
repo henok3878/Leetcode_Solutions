eb64cbac26b024f53a841445785d86a061c86d95
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-20 16:08:40 +0300

    Time: 71 ms (26.52%), Space: 14.1 MB (19.70%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -4,12 +4,13 @@ class Solution:
         n = len(obstacleGrid)
         m = len(obstacleGrid[0])
         
+        dp = [[None for _ in range(m)] for _ in range(n)]
         
-        @cache
         def dfs(i,j):
             if i == n - 1 and j == m - 1:
                 return 1
-            
+            if dp[i][j] != None:
+                return dp[i][j]
             down = 0
             if(0 <= i + 1 < n and obstacleGrid[i][j] != 1):
                 down = dfs(i + 1, j)
@@ -18,7 +19,9 @@ class Solution:
             if(0 <= j + 1 < m and obstacleGrid[i][j] != 1):
                 right = dfs(i, j + 1)
             
-            return down + right
+            dp[i][j] = down + right
+            
+            return dp[i][j]
          
         if(obstacleGrid[0][0] == 1 or obstacleGrid[n-1][m-1] == 1):
             return 0
