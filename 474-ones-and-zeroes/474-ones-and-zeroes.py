3e3c5735de045e55ee53f6c142a951acfeb7e649
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-05-23 11:13:43 +0300

    Time: 2947 ms (86.89%), Space: 288 MB (13.37%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        
+        def counter(s):
+            n = len(s)
+            zeros = 0
+            ones = 0
+            for i in range(n):
+                if s[i] == "0":
+                    zeros += 1
+                else:
+                    ones += 1
+            return (zeros,ones)
+        
+        count = []
+        MIN = -1*(10 ** 20)
+        for s in strs:
+            count.append(counter(s))
+        
+        @cache 
+        def dfs(i,curr):
+            if i == len(strs):
+                return 0
+            nonlocal count
+            select = MIN 
+            if(curr[0] + count[i][0] <= m and curr[1] + count[i][1] <= n):
+                select = 1 + dfs(i + 1, (curr[0] + count[i][0], curr[1] + count[i][1]))
+            skip = dfs(i + 1, curr)
+            
+            return max(select,skip)
+        
+        return dfs(0,(0,0))
+        
+        
+    
+            
