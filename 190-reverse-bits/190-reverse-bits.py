3748325ae805442adfbb199d81f54ea3693b3130
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-19 21:21:03 +0300

    Time: 59 ms (24.25%), Space: 13.9 MB (49.16%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -4,7 +4,7 @@ class Solution:
         # traversing bits of 'n' from the right
         for i in range(32):
             rev = rev << 1
-            rev |= n & 1
+            rev += n & 1
             n = n >> 1
 
         return rev
