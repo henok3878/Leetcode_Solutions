293acb8c64a9a267fc40a57809e31d047c23d0b2
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-20 13:36:23 +0300

    Time: 81 ms (5.46%), Space: 13.8 MB (63.40%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -4,23 +4,20 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         pivot = 1
-        redwhite = 0
-        blue = len(nums) - 1
-        while redwhite < blue:
-            if nums[redwhite] == 2:
-                nums[redwhite],nums[blue] = nums[blue],nums[redwhite]
-                blue -= 1
-            else:
-                redwhite += 1
-        if nums[blue] == 2:
-            blue -= 1
-        white = blue 
         red = 0
-        while red < white:
-            if nums[red] == 1:
+        white = 0
+        blue = len(nums) - 1
+        
+        while white < len(nums):
+            #print(nums,red,white,blue)
+            if nums[white] == 2 and blue > white:
+                nums[white],nums[blue] = nums[blue],nums[white]
+                blue -= 1
+            elif nums[white] == 0:
                 nums[red],nums[white] = nums[white],nums[red]
-                white -= 1
-            else:
                 red += 1
+                white += 1
+            else:
+                white += 1
         
                 
