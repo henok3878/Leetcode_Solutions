53b8f5dfde904933eb6a74494cbe40b19238e76d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-20 10:18:31 +0300

    Time: 32 ms (88.49%), Space: 13.8 MB (96.57%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -4,9 +4,7 @@ class Solution:
             return 0
         elif n < 3:
             return 1
-        ppp = 0
-        pp = 1
-        p = 1
+        ppp,pp,p = 0,1,1
         for i in range(3,n + 1):
             curr = ppp + pp + p
             ppp,pp,p = pp,p,curr
