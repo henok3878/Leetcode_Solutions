3680f47dc3c2ffa8c8b12e8fd5ecfe33c98fe2d7
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-20 09:59:27 +0300

    Time: 45 ms (54.20%), Space: 14.1 MB (19.15%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,20 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [-1] * n
-        def rec(i):
-            if i < 0:
-                return 0
-            if(dp[i] != -1):
-                return dp[i]
+        @cache
+        def helper(i):
+            if i == 0:
+                return nums[0]
+            elif i == 1:
+                return max(nums[0],nums[1])
             
-            skip = rec(i-1)
-            select = nums[i] + rec(i-2)
-            
-            dp[i] = max(skip,select)
-            
-            return dp[i]
+            return max(helper(i-1),helper(i-2) + nums[i])
         
-        return rec(n-1)
-                
-                
+        return helper(n-1)
+            
