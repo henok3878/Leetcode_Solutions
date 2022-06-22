4e4d36836153f86b9b01f98da356c57669a3523c
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-22 23:14:18 +0300

    Time: 28 ms (99.70%), Space: 14 MB (57.05%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,23 +1,19 @@
 class Solution:
+    # O(n)*O(k) + O(n) = O(n)*O(k)
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        actual_indices = [-1] * len(sources)
-        for i,sub_str in enumerate(sources):
-            actual_indices[i] = s.find(sub_str,indices[i]) 
         maps = defaultdict(lambda:-1)
-        for i,idx in enumerate(indices):
-            maps[idx] = i
+        # O(k)*O(n)
+        for i,sub_str in enumerate(sources): # O(k)
+            if s.find(sub_str,indices[i]) == indices[i]: # O(n)
+                maps[indices[i]] = i
+                  
         idx = 0
         ans = ""
-        while idx < len(s):
+        while idx < len(s): # O(n)
             if maps[idx] != -1:
                 pos = maps[idx]
-                if indices[pos] == actual_indices[pos]:
-                    sub_str = sources[pos]
-                    ans += targets[pos]
-                    idx += len(sub_str)
-                else:
-                    ans += s[idx]
-                    idx += 1
+                ans += targets[pos]
+                idx += len(sources[pos])
             else:
                 ans += s[idx]
                 idx += 1
