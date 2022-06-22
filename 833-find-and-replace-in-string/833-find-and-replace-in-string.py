8a9328dd3cc0a8ef632d3f031bc225aaef6db345
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-22 23:12:23 +0300

    Time: 62 ms (35.26%), Space: 14 MB (57.05%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,17 +1,23 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        maps = defaultdict(lambda:-1)
+        actual_indices = [-1] * len(sources)
         for i,sub_str in enumerate(sources):
-            if s.find(sub_str,indices[i]) == indices[i]:
-                maps[indices[i]] = i
-                  
+            actual_indices[i] = s.find(sub_str,indices[i]) 
+        maps = defaultdict(lambda:-1)
+        for i,idx in enumerate(indices):
+            maps[idx] = i
         idx = 0
         ans = ""
         while idx < len(s):
             if maps[idx] != -1:
                 pos = maps[idx]
-                ans += targets[pos]
-                idx += len(sources[pos])
+                if indices[pos] == actual_indices[pos]:
+                    sub_str = sources[pos]
+                    ans += targets[pos]
+                    idx += len(sub_str)
+                else:
+                    ans += s[idx]
+                    idx += 1
             else:
                 ans += s[idx]
                 idx += 1
