8a70a1d0f62c7ba34fafee0855a02a0b5813347b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-23 13:36:23 +0300

    Time: 592 ms (98.84%), Space: 16.3 MB (36.36%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -7,10 +7,7 @@ class Solution:
                 if matrix[i][j] == 1:
                     adj_min = min(matrix[i-1][j-1],matrix[i-1][j], matrix[i][j-1]) if i -1 >= 0 and j - 1 >= 0 else 0
                     matrix[i][j] = adj_min + 1
-        for i in range(m):
-            for j in range(n):
                     total += matrix[i][j]
-        #print(matrix)
         return total
     
     
