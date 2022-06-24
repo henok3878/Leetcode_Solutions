7ec852aeb22d09601d1fe0b8beaa01250759b248
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-24 15:24:13 +0300

    Time: 70 ms (14.13%), Space: 14 MB (29.33%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -4,15 +4,14 @@ class Solution:
         Do not return anything, modify matrix in-place instead.
         """
         n = len(matrix) 
-        MAX = 5000
-        for i in range(n):
+        def transpose():
             for j in range(n):
-                x,y = i,j 
-                while matrix[x][y] > -1001 and 0 <= x < n and 0 <= y < n:
-                    matrix[x][y], matrix[y][n-1-x]  = matrix[y][n-1-x],matrix[x][y] - MAX
-                    x,y = y, n - 1 - x 
+                for i in range(j):
+                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
                     
-        for i in range(n):
-            for j in range(n):
-                if matrix[i][j] < -1001:
-                    matrix[i][j] += MAX
+        def reflect():
+            for i in range(n):
+                matrix[i].reverse()
+        
+        transpose()
+        reflect()
