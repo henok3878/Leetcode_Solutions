54a941a9fbdda5ca0c7df79cfc156546673b1b41
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-06-30 23:25:47 +0300

    Time: 63 ms (68.52%), Space: 13.9 MB (30.52%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -3,14 +3,10 @@ class Solution:
         
         mapping = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M" : 1000}
         n = len(s)
-        result = 0
-        i = 0
-        while i < n-1:
+        result = mapping[s[-1]]
+        for i in range(n-2,-1,-1):
             if mapping[s[i]] >= mapping[s[i + 1]]:
                 result += mapping[s[i]]
-                i += 1
             else:
-                result += mapping[s[i + 1]] - mapping[s[i]]
-                i += 2 
-        result += mapping[s[n-1]] if n == 1 or mapping[s[n-2]] >= mapping[s[n-1]] else 0
+                result -= mapping[s[i]]
         return result
