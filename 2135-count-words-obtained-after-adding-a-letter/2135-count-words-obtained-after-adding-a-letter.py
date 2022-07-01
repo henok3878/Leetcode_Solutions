7b4bf45a477eb0b4ed963e8f73a9e854bdc91bd8
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-07-01 15:03:40 +0300

    Time: 2170 ms (36.16%), Space: 40.1 MB (16.82%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -3,14 +3,14 @@ class Solution:
         
         startWordsSorted = [tuple(sorted(x)) for x in startWords]
         starts = set(startWordsSorted)
-        ans = []
+        ans = 0
         for word in targetWords:
             for i in range(len(word)):
                 word = sorted(word)
                 temp = word[:i] + word[i + 1:]
                 if tuple(temp) in starts:
-                    ans.append(word)
+                    ans += 1
                     break
         
-        return len(ans)
+        return ans
                 
