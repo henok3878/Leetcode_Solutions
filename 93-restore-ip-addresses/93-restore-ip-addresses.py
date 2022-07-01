24139c7c1847c6f3d23cf48a2e6356345fc742bd
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-07-01 09:39:27 +0300

    Time: 62 ms (38.44%), Space: 13.8 MB (82.31%) - LeetHub

--- 93-restore-ip-addresses/93-restore-ip-addresses.py
+++ 93-restore-ip-addresses/93-restore-ip-addresses.py
@@ -6,25 +6,25 @@ class Solution:
             elif int(num) > 255:
                 return False
             return True
-        def restoreIpAddressesHelper(idx,dots,sofar):
-            if idx >= len(s):
-                return
-            elif dots == 3:
-                num4 = s[idx:]
-                if is_valid(num4):
-                    sofar.append(num4)
-                    ans.append(".".join(sofar))
-                    sofar.pop()
-            for i in range(idx + 1, len(s)):
-                num = s[idx:i]
-                if not is_valid(num):
-                    return 
-                sofar.append(num)
-                restoreIpAddressesHelper(i,dots + 1,sofar)
-                sofar.pop()
-            
         ans = []
-        restoreIpAddressesHelper(0,0,[])            
+        n = len(s)
+        for fst in range(1,n):
+            num1 = s[:fst]
+            if not is_valid(num1):
+                continue
+            for snd in range(fst + 1, n):
+                num2 = s[fst:snd]
+                if not is_valid(num2):
+                    continue
+                for thrd in range(snd + 1, n):
+                    num3 = s[snd:thrd]
+                    if not is_valid(num3):
+                        continue
+                    num4 = s[thrd:]
+                    if not is_valid(num4):
+                        continue
+                    ans.append(num1 + "." + num2 + "." + num3 + "." + num4)
+                    
         return ans
                         
         
