55630fa950902c8add371905f00c5336f3de0eff
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-07-01 13:56:13 +0300

    Time: 87 ms (6.09%), Space: 13.9 MB (57.14%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.py
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -1,20 +1,20 @@
 class Solution:
-    # O(n)*O(k) + O(n) = O(n)*O(k)
-    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        maps = defaultdict(lambda:-1)
-        # O(k)*O(n)
-        for i,sub_str in enumerate(sources): # O(k)
-            if s.find(sub_str,indices[i]) == indices[i]: # O(n)
-                maps[indices[i]] = i
-                  
-        idx = 0
-        ans = ""
-        while idx < len(s): # O(n)
-            if maps[idx] != -1:
-                pos = maps[idx]
-                ans += targets[pos]
-                idx += len(sources[pos])
-            else:
-                ans += s[idx]
-                idx += 1
-        return ans
+    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:     
+        i = 0 
+        n = len(s)
+        res = []
+        idxs = set(indices)
+        while i < n:
+            found = False
+            for j,idx in enumerate(indices):
+                if idx == i and idx + len(sources[j]) <= n:
+                    temp = s[idx: idx + len(sources[j])]
+                    if temp == sources[j]:
+                        res.append(targets[j])
+                        i += len(sources[j])
+                        found = True 
+                        break
+            if not found:
+                res.append(s[i])
+                i += 1      
+        return "".join(res)
