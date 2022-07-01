6956ad7d8d84b85f085f841ceff18904ae9a4162
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-07-01 13:14:55 +0300

    Time: 139 ms (63.78%), Space: 19.9 MB (10.05%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        strs_copy = []
-        for s in strs:
-            strs_copy.append(''.join(sorted(s)))
-        
         maps = defaultdict(list)
-        for i,s in enumerate(strs_copy):
-            maps[s].append(strs[i])
+        for s in strs:
+            counter = [0] * 26
+            for c in s:
+                counter[ord(c) - ord('a')] += 1 
+            maps[tuple(counter)].append(s)
             
         return maps.values()
         
