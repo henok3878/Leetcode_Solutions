c4ada6beee4ff1d3983e07e4cb497f182e4379bb
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-07-02 16:32:08 +0300

    Time: 693 ms (93.10%), Space: 136.9 MB (56.05%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        
+
+        
+        def find_node(val, node, path):
+            if not node:
+                return None
+            if node.val == val:
+                return path
+            path.append("R")
+            right = find_node(val, node.right, path)
+            if right:
+                return right 
+            path.pop()
+            path.append("L")
+            left = find_node(val, node.left, path)
+            if left:
+                return left
+            path.pop()
+            
+        path1 = find_node(startValue,root,[])
+        path2 = find_node(destValue,root,[])
+        
+        ans = []
+        i = 0
+        j = 0 
+        while i < len(path1) and j < len(path2):
+            if path1[i] == path2[j]:
+                i += 1 
+                j += 1
+            else:
+                break
+        for idx in range(i,len(path1)):
+            ans.append('U')
+        for idx in range(j, len(path2)):
+            ans.append(path2[idx])
+            
+        return "".join(ans)
+        
