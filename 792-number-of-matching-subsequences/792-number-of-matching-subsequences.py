3fdc0aaa57a88d2acc755f2563025760e8277c2d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-07-02 18:12:52 +0300

    Time: 587 ms (70.40%), Space: 15.5 MB (75.34%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.py
@@ -1,22 +1,22 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        buckets = defaultdict(list)
         
-        def is_subsequence(word):
-            last = -1 
-            for c in word:
-                idx = bisect_right(char_to_indices[c],last)
-                if idx >= len(char_to_indices[c]):
-                    return False
-                else:
-                    last = char_to_indices[c][idx]
-            return True
-        
-        char_to_indices = defaultdict(list)
-        for i,c in enumerate(s):
-            char_to_indices[c].append(i) 
-            
-        valid = 0 
         for word in words:
-            if is_subsequence(word):
-                valid += 1 
-        return valid 
+            starting_char = word[0]
+            buckets[starting_char].append(word)
+            
+        counter = 0   
+        for c in s:
+            curr_words = buckets[c]
+            buckets[c] = []
+            for word in curr_words:
+                suffix = word[1:]
+                if len(suffix) == 0:
+                    counter += 1 
+                else:
+                    starting_char = suffix[0]
+                    buckets[starting_char].append(suffix)
+        
+        
+        return counter 
