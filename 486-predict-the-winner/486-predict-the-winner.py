db0e8b044cc6b9a31c70ca33897043372aaec332
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-01 15:25:05 +0300

    Time: 52 ms (65.19%), Space: 13.8 MB (84.86%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -4,12 +4,11 @@ class Solution:
         n = len(nums)
         dp = [0] * n  
         for l in range(n-1,-1,-1): 
-            temp = dp[:] 
             for r in range(n):
                 if l > r: 
                     dp[r] = 0 
                 else:
-                    left = temp[r] if l + 1 < n else 0 
+                    left = dp[r] if l + 1 < n else 0 
                     right = dp[r-1] if r - 1 >= 0 else 0 
                     dp[r] = max(nums[l] - left, nums[r] - right)
                     
