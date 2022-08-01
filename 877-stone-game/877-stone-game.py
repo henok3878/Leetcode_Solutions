9e06af2bfffe0103b8546622c8ac97a7bda3d128
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-01 09:48:33 +0300

    Time: 877 ms (27.58%), Space: 126.1 MB (5.66%) - LeetHub

---
+++ 877-stone-game/877-stone-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        
+        
+        @cache 
+        def helper(l,h):
+            if l >= h:
+                return 0 
+            lower = helper(l + 1, h) + piles[l] 
+            higher = helper(l, h - 1) + piles[h] 
+            
+            return max(lower, higher)
+        
+        return helper(0, len(piles) -1 )
