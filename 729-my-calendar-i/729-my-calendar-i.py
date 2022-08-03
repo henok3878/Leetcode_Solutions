76da902e3ff5f590991feb02d559fb59623b7853
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-03 16:04:23 +0300

    Time: 423 ms (56.54%), Space: 15.3 MB (10.23%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.py
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -9,19 +9,10 @@ class MyCalendar:
     def book(self, start: int, end: int) -> bool:
         
         idx = self.books.bisect_left((start,end))
-        #print(self.books,start,end, idx)
-        if idx == len(self.books):
-            st_prev, end_prev = self.books[idx - 1] if idx > 0 else (-1,-1) 
-            #print("idx == len", st_prev, end_prev,start,end)
-            if start < end_prev: 
-                return False 
-        else:
-            
-            st_prev, end_prev = self.books[idx - 1] if idx > 0 else (-1,-1) 
-            st_next, end_next = self.books[idx] 
-            #print(st_prev,end_prev, st_next, end_next)
-            if start < end_prev or end > st_next:
-                return False 
+        st_prev, end_prev = self.books[idx - 1] if idx > 0 else (-1,-1) 
+        st_next, end_next = self.books[idx] if idx < len(self.books) else  (float('inf'),float('inf'))
+        if start < end_prev or end > st_next:
+            return False 
         self.books.add((start,end))
         return True 
 
@@ -29,16 +20,3 @@ class MyCalendar:
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
 # param_1 = obj.book(start,end)
-
-
-
-"""
-
-[10,20], [15,25] , [20,30] 
-
-
-[st1 , end1) 
-
-[st2, end2)
-
-"""
