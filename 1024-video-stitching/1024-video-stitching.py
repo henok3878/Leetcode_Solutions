b93686fbed3166b79740b91727ac5884b29083dc
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-13 15:40:31 +0300

    Time: 63 ms (32.87%), Space: 14.1 MB (8.74%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.py
+++ 1024-video-stitching/1024-video-stitching.py
@@ -3,23 +3,32 @@ class Solution:
         
         N = len(clips)
         
-        @cache 
-        def helper(idx, covered_sofar):
-            if covered_sofar >= time:
-                return 0 
-            elif idx >= N:
-                return float('inf')
-            
-            ret = float('inf') 
-            if clips[idx][0] <= covered_sofar:
-                ret = 1 + helper(idx + 1, clips[idx][1]) 
-            
-            return min(ret, helper(idx + 1, covered_sofar))
+        
         
         clips.sort() 
         if clips[0][0] > 0:
             return -1 
-        res = helper(0,0)
+        dp = [[float('inf')] * (time + 1) for _ in range(N + 1)]
+        for i in range(N + 1):
+            dp[i][time] = 0 
         
+        for i in range(N - 1, -1,-1):
+            for c in range(time, -1,-1):
+                if clips[i][0] <= c:
+                    nxt = clips[i][1] if clips[i][1] <= time else time
+                    dp[i][c] = 1 + dp[i + 1][nxt]
+                
+                dp[i][c] = min(dp[i][c], dp[i + 1][c])
+        
+        res = dp[0][0]
         return res if res != float('inf') else -1
+    
+    
+    """
+                            i,c     
+        
+    i + 1, clips[i][1]              i + 1, c 
+    
+    
+    """
         
