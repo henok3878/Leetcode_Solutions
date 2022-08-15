f5e74acbd3ff36be016ce52799ccdc6e843f75c2
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-15 13:51:56 +0300

    Time: 243 ms (52.76%), Space: 17.6 MB (37.22%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,61 +1,24 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        """
-        
-        Minumum: 
-        
-        Thinking what not to touch in word1: Don't touch longest common subsequence 
-        
-        "horse", "ros"
-        
-        LCS = rs 
-        
-        - len(word1) - len(word2) delations is needed 
-        - len(word2) - len(lcs): replacment 
-        
-        Won't work: 
-            -> ex: orse , ros 
-                - lcs = rs 
-                - ors
-                        
-        h o r s e
-            
-        - - -  , r and s are selected and 2 delations performed 
-        
-        
-        h o r s e,  r o s 
-        ^           ^ 
-        
-        if word1[i] != word2[j]:
-            # match them by replacing or inserting or deleting 
-        
-        else: 
-            # curr char matched 
-            word1[i + 1], word2[i + 1]
-        
-        
-        
-        """
-        
-        
-        @cache 
-        def helper(i,j):
 
-            if j == len(word2):
-                return len(word1) - i 
-            elif i == len(word1):
-                return len(word2) - j
             
-            res = float('inf')
-            if word1[i] == word2[j]:
-                res = min (res, helper( i + 1, j + 1) ) 
-            else:
-                res = min(helper(i,j + 1), helper(i + 1, j), helper(i + 1, j + 1)) + 1
-            
-            return res 
+        N,M = len(word1), len(word2) 
         
-        return helper(0,0)
-            
+        dp = [[0] * (M + 1) for _ in range(N + 1)] 
+        
+        for j in range(M + 1):
+            dp[N][j] = M - j 
+        for i in range(N + 1):
+            dp[i][M] = N - i 
+        
+        for i in range(N - 1,-1,-1):
+            for j in range(M -1,-1,-1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = min(dp[i][j + 1], dp[i + 1][j], dp[i + 1][j + 1]) + 1 
+        
+        return dp[0][0]
         
         
         
