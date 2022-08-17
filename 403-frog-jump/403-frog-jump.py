0f0a7870f5005624ef4f36bd88a448c39f035d9b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-17 14:05:44 +0300

    Time: 116 ms (98.37%), Space: 24.7 MB (16.73%) - LeetHub

--- 403-frog-jump/403-frog-jump.py
+++ 403-frog-jump/403-frog-jump.py
@@ -2,28 +2,21 @@ class Solution:
     def canCross(self, stones: List[int]) -> bool:
         
         last = stones[-1] 
-        s_i = defaultdict(lambda: -1) 
-        
-        for i,stn in enumerate(stones):
-            s_i[stn] = i 
-        
-        if stones[0] - stones[1] > 1:
-            return False     
-        
+        stones_unit = set(stones)
         @cache 
-        def helper(idx,prev):
-            if idx == len(stones)-1:
+        def helper(unit,prev):
+            if unit == last:
                 return True 
             if prev == 0:
-                next_unit = stones[idx] + 1
-                if s_i[next_unit] != -1:
-                    if helper(s_i[next_unit], 1):
+                next_unit = unit + 1
+                if next_unit in stones_unit:
+                    if helper(next_unit, 1):
                         return True 
                 return False 
             for jump in range(prev - 1, prev + 2):
-                next_unit = stones[idx] + jump
-                if s_i[next_unit] != -1:
-                    if helper(s_i[next_unit], jump):
+                next_unit = unit + jump
+                if next_unit in stones_unit:
+                    if helper(next_unit,jump):
                         return True 
             return False 
         
