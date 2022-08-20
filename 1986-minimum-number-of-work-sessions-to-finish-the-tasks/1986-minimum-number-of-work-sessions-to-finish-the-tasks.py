f65835c2018f7be4515029fd67def99f066087ee
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-20 22:30:46 +0300

    Time: 73 ms (93.57%), Space: 13.8 MB (91.90%) - LeetHub

---
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minSessions(self, tasks: List[int], sessionTime: int) -> int:
+        
+        
+        def is_possible(idx):
+            if idx == n:
+                return True 
+            
+            for i in range(mid):
+                if slots[i] - tasks[idx] < 0:
+                    continue 
+                slots[i] -= tasks[idx]
+                if is_possible(idx + 1):
+                    return True 
+                slots[i] += tasks[idx] 
+                if slots[i] == sessionTime:
+                    break
+            return False 
+            
+            
+        n = len(tasks) 
+        tasks.sort(reverse = True) 
+        l,r = 1, n 
+        ans = n 
+        while l <= r:
+            mid = (l + r) // 2 
+            #print(l,r,mid)
+            slots = [sessionTime] * mid 
+            if is_possible(0):
+                r = mid - 1
+                ans = mid 
+            else:
+                l = mid + 1 
+        return l 
+    
+    
+    """    
+    
+    """
