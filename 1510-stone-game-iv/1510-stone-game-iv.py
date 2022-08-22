074f2a957ec62de8e812a8883bbe24fe454e1f8f
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-22 20:37:14 +0300

    Time: 3631 ms (28.20%), Space: 14.8 MB (80.12%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,16 +1,16 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
         
-        dp = [(False,True)] * (n + 1)  
-        dp[1] = (True,False)
+        dp = [False] * (n + 1)  
+        dp[1] = True
         
         for i in range(2, n + 1):
             for r in range(1, int(i ** 0.5) + 1):
                 curr = r * r 
                 if curr <= i:
-                    if dp[i-curr][1]:
-                        dp[i] = True,False    
+                    if not dp[i-curr]:
+                        dp[i] = True  
                 else:
                     break 
-        return dp[n][0]
+        return dp[n]
         
