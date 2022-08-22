15c8a7d9c3ec14651bee913d25273d3419d0c7cb
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-22 13:53:08 +0300

    Time: 1646 ms (5.20%), Space: 17.9 MB (5.50%) - LeetHub

--- 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.py
+++ 1227-airplane-seat-assignment-probability/1227-airplane-seat-assignment-probability.py
@@ -1,3 +1,12 @@
 class Solution:
     def nthPersonGetsNthSeat(self, n: int) -> float:
-        return 0.5 if n != 1 else 1.0
+        if n < 2:
+            return 1 
+        dp = [0] * ( n + 1)  
+        dp[1] = 1 
+        dp[2] = 1 / n;
+        
+        for i in range(3,n + 1):
+            dp[i] = dp[i - 1]
+            dp[i] += dp[i-1]/ (n - (i - 2))
+        return 1 - dp[n]
