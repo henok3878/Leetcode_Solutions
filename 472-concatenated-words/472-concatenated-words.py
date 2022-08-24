9d0cc8f42ee2ee2c6684b58d242f97470570065b
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-24 03:16:08 +0300

    Time: 2490 ms (5.03%), Space: 41.4 MB (7.19%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -12,33 +12,25 @@ class Solution:
             curr.isWord = True 
         
         def find(word,count):
-            nonlocal ans 
             curr = root
             for i,char in enumerate(word): 
                 idx = ord(char) - ord('a') 
                 if curr.isWord:
-                    #print(word,word[:i],word[i:]) 
                     if find(word[i:], count + 1):
                         return True 
                 if curr.children[idx] is None:
                     return False 
                 curr = curr.children[idx]
-            #print("end",word, curr.isWord, count + 1)
-            res =  curr.isWord and count + 1 >= 2 
-            if res:
-                ans += 1 
-            return res 
-        ans = 0
-        res = []
+            return curr.isWord and count + 1 >= 2 
+        
+        ans = [] 
         for word in words:
             insert(word)         
             
         for word in words:
-            temp = ans 
-            find(word, 0)
-            if temp + 1 == ans:
-                res.append(word) 
-        return res 
+            if find(word,0):
+                ans.append(word)
+        return ans
                     
         
 class TrieNode:
