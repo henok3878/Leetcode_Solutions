311c6135f88d713b6c9f7ab610ea9a2b8f48bd28
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-26 01:19:16 +0300

    Time: 4790 ms (5.01%), Space: 156.1 MB (5.00%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.py
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.py
@@ -1,38 +1,23 @@
-class WordFilter:
-    
-    #O(N * K^2)
-    def __init__(self, words: List[str]):
-        self.root = TrieNode() 
-        for i,word in enumerate(words):
-            for j in range(len(word)-1,-1,-1):
-                suf_word = word[j:len(word)] + '-' + word 
-                self.__insert(i,suf_word) 
-    
-    #O(len(word))
-    def __insert(self,i, word):
-        curr = self.root 
-        for char in word: 
-            if char not in curr.kids:
-                curr.kids[char] = TrieNode() 
-            curr = curr.kids[char]
-            curr.idx = i
-    #O(len(word))
-    def __find(self, word):
-        curr = self.root 
-        for char in word:
-            if char not in curr.kids:
-                return -1 
-            curr = curr.kids[char] 
-        return curr.idx 
-    #O(len(word) * Q)
-    def f(self, pref: str, suff: str) -> int:
-        return self.__find(suff + "-" + pref)
-        
-class TrieNode:
-    def __init__(self):
-        self.kids = {}
-        self.idx = -1 
+Trie = lambda: collections.defaultdict(Trie)
+WEIGHT = False
 
-# Your WordFilter object will be instantiated and called as such:
-# obj = WordFilter(words)
-# param_1 = obj.f(pref,suff)
+class WordFilter:
+    def __init__(self, words: List[str]):
+        self.trie = Trie()
+
+        for weight, word in enumerate(words):
+            word += '#'
+            for i in range(len(word)):
+                cur = self.trie
+                cur[WEIGHT] = weight
+                for j in range(i, 2 * len(word) - 1):
+                    cur = cur[word[j % len(word)]]
+                    cur[WEIGHT] = weight
+
+    def f(self, pref: str, suff: str) -> int:
+        cur = self.trie
+        for letter in suff + '#' + pref:
+            if letter not in cur:
+                return -1
+            cur = cur[letter]
+        return cur[WEIGHT]
