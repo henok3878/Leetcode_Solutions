a78d440be19e9a94d32dbb1b08c51c1c433788f2
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-28 01:07:23 +0300

    Time: 113 ms (81.19%), Space: 15.5 MB (56.72%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
@@ -6,20 +6,11 @@
 #         self.right = right
 class Solution:
     def sufficientSubset(self, root: Optional[TreeNode], limit: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        elif root.left == root.right: 
+            return None if limit > root.val else root 
+        root.left = self.sufficientSubset(root.left, limit - root.val) 
+        root.right = self.sufficientSubset(root.right,limit - root.val) 
         
-        
-        def dfs(node,path_sum):
-            if node.left is None and node.right is None:
-                return path_sum + node.val 
-            
-            left = dfs(node.left, path_sum + node.val) if node.left else float('-inf')
-            right = dfs(node.right, path_sum + node.val)  if node.right else float('-inf')
-            if left < limit:
-                node.left = None 
-            if right < limit: 
-                node.right = None 
-            return max(left, right)
-        
-        dummy = TreeNode(val = 0,left = None, right = root)
-        res = dfs(dummy,0)
-        return dummy.right
+        return root if root.left != root.right else None 
