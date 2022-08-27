d9c4d8a83b8d01f7f449aad3ac36040fedde70e4
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-28 00:59:49 +0300

    Time: 187 ms (19.11%), Space: 15.5 MB (18.51%) - LeetHub

--- 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
+++ 1080-insufficient-nodes-in-root-to-leaf-paths/1080-insufficient-nodes-in-root-to-leaf-paths.py
@@ -11,6 +11,7 @@ class Solution:
         def dfs(node,path_sum):
             if node.left is None and node.right is None:
                 return path_sum + node.val 
+            
             left = dfs(node.left, path_sum + node.val) if node.left else float('-inf')
             right = dfs(node.right, path_sum + node.val)  if node.right else float('-inf')
             if left < limit:
@@ -19,7 +20,6 @@ class Solution:
                 node.right = None 
             return max(left, right)
         
-        res = dfs(root,0)
-        if res < limit:
-            return None
-        return root
+        dummy = TreeNode(val = 0,left = None, right = root)
+        res = dfs(dummy,0)
+        return dummy.right
