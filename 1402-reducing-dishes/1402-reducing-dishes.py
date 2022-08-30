331fdcff1db89e6955646952358d8ce85453db17
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-08-30 17:25:00 +0300

    Time: 1503 ms (5.31%), Space: 19.8 MB (13.81%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,14 +1,14 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort() 
+        n = len(satisfaction)
+        dp = [[0] * (n + 2) for _ in range(n + 1)]
+        for i in range(n -1,-1,-1):
+            for j in range(n,0,-1):
+                dp[i][j] = max(dp[i + 1][j], dp[i + 1][j + 1] + satisfaction[i] * j)
         
-        @cache 
-        def helper(i,time):
-            if i == len(satisfaction):
-                return 0 
-            skip = helper(i + 1, time)
+        return dp[0][1]
+    """
+    skip = helper(i + 1, time)
             select = helper(i + 1, time + 1) + (satisfaction[i] * time) 
-            
-            return max(skip,select)
-        
-        return helper(0,1)
+    """
