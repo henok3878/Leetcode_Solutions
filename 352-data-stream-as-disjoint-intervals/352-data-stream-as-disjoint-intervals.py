dc99bec66e8069313d3970e737a3b7249423fa79
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-09-02 06:16:58 +0300

    Time: 396 ms (23.63%), Space: 19.4 MB (17.03%) - LeetHub

--- 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
+++ 352-data-stream-as-disjoint-intervals/352-data-stream-as-disjoint-intervals.py
@@ -3,39 +3,26 @@ from sortedcontainers import SortedList
 class SummaryRanges:
 
     def __init__(self):
-        self.list = SortedList() 
+        self.list = SortedList([[float('-inf'),float('-inf')], [float('inf'),float('inf')]]) 
 
     def addNum(self, val: int) -> None:
-        if len(self.list) == 0:
-            self.list.add([val,val]) 
-            return 
         idx = self.list.bisect_left([val,val]) 
-        if idx == len(self.list):
-            if self.list[-1][1] >= val-1:
-                self.list[-1][1] = max(val,self.list[-1][1]) 
-            else:
-                self.list.add([val,val]) 
-        elif idx == 0:
-            if self.list[0][0] == val or self.list[0][0] == val + 1:
-                self.list[0][0] = val 
-            else:
-                self.list.add([val,val]) 
-        else: 
-            prev = self.list[idx - 1]
-            nxt = self.list[idx] 
-            if prev[1] == val - 1 and nxt[0] == val + 1:
-                prev[1] = nxt[1]
-                self.list.pop(idx)
-            elif prev[1] >= val - 1: 
-                prev[1] = max(val,prev[1]) 
-            elif nxt[0] == val or nxt[0] == val + 1:
-                nxt[0] = min(val, nxt[0])
-            else:
-                self.list.add([val,val])
+
+        prev = self.list[idx - 1]
+        nxt = self.list[idx] 
+        if prev[1] == val - 1 and nxt[0] == val + 1:
+            prev[1] = nxt[1]
+            self.list.pop(idx)
+        elif prev[1] >= val - 1: 
+            prev[1] = max(val,prev[1]) 
+        elif nxt[0] == val or nxt[0] == val + 1:
+            nxt[0] = min(val, nxt[0])
+        else:
+            self.list.add([val,val])
         
 
     def getIntervals(self) -> List[List[int]]:
-        return self.list         
+        return self.list[1:-1]        
 
 
 # Your SummaryRanges object will be instantiated and called as such:
