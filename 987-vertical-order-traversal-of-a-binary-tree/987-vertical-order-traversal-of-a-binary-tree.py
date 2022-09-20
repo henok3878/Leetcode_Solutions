870d1e5cb380f91e08a217eea0ad8d055cfeed7f
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-09-20 23:45:33 +0300

    Time: 42 ms (81.17%), Space: 14.7 MB (6.14%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from sortedcontainers import SortedList 
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        offset_l = 0
+        offset_r = 0  
+        grid = defaultdict(lambda: SortedList())
+        def helper(node,row,col):
+            nonlocal offset_l 
+            nonlocal offset_r
+            
+            if not node:
+                return 
+            offset_l = min(offset_l, col) 
+            offset_r = max(offset_r,col)
+            grid[col].add((row,node.val))
+            helper(node.left, row + 1, col - 1) 
+            helper(node.right, row + 1, col + 1)
+            
+        helper(root,0,0) 
+        ans = []
+        for col in range(offset_l, offset_r + 1):
+            if col in grid:
+                curr = []
+                for r,v in grid[col]:
+                    curr.append(v) 
+                ans.append(curr) 
+        return ans 
+            
