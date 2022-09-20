7ab7ebd3087f3ba54f7d2d1b55bad6dfdb403f66
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-09-20 21:28:58 +0300

    Time: 56 ms (38.89%), Space: 14 MB (26.39%) - LeetHub

--- 880-decoded-string-at-index/880-decoded-string-at-index.py
+++ 880-decoded-string-at-index/880-decoded-string-at-index.py
@@ -1,6 +1,5 @@
 class Solution:
     def decodeAtIndex(self, s: str, k: int) -> str:
-        k -= 1 
         total_len = 0
         for c in s:
             if c.isdigit():
@@ -12,10 +11,10 @@ class Solution:
             c = s[i] 
             if c.isdigit():
                 total_len //= int(c) 
-                if k + 1 > total_len:
+                if k > total_len:
                     k %= total_len 
             else:
-                if k + 1 == total_len:
+                if k == total_len or k == 0:
                     return c 
                 else:
                     total_len -= 1 
