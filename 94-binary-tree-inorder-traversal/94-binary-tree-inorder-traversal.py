eac95177856555eab20a7fca8334b9a311bdacad
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-09-21 16:24:18 +0300

    Time: 70 ms (5.94%), Space: 13.9 MB (13.02%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -15,10 +15,15 @@ class Solution:
             if nxt == 0:
                 if not node:
                     continue 
+                stack.append((node,1)) 
+            elif nxt == 1:
                 stack.append((node,2))
                 stack.append((node.left,0)) 
+                
             elif nxt == 2:
                 ans.append(node.val)
+                stack.append((node,3))
+            else:
                 stack.append((node.right, 0)) 
         
         return ans 
