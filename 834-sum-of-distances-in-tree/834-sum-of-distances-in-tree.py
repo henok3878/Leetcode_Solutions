d1d9f1a74799ae062f939579300cd8ecb15fc84d
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-09-23 12:50:34 +0300

    Time: 2692 ms (6.48%), Space: 66.8 MB (18.75%) - LeetHub

---
+++ 834-sum-of-distances-in-tree/834-sum-of-distances-in-tree.py
@@ -0,0 +1,35 @@
+class Solution:
+    def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
+        sub_tree = [0] * n 
+        sum_dist = [0] * n 
+        
+        tree = defaultdict(list) 
+        for u,v in edges:
+            tree[u].append(v) 
+            tree[v].append(u) 
+        
+        def dfs1(node, p):
+            sub_tree[node] = 1 
+            for adj in tree[node]:
+                if adj != p:
+                    dfs1(adj,node) 
+                    sub_tree[node] += sub_tree[adj] 
+                    sum_dist[node] += (sum_dist[adj] + sub_tree[adj])
+        def dfs2(node,p):
+            for adj in tree[node]:
+                if adj != p:
+                    adj_size = sub_tree[adj] 
+                    out_side_adj = n - adj_size 
+                    adj_contb = sum_dist[adj] + sub_tree[adj] 
+                    p_total = sum_dist[node] 
+                    diff = p_total - adj_contb 
+                    sum_dist[adj] += (diff) + out_side_adj
+                    dfs2(adj,node) 
+                    
+        dfs1(0,-1) 
+        # print(sub_tree)
+        # print(sum_dist)
+        dfs2(0,-1)
+        # print(sum_dist)
+            
+        return sum_dist
