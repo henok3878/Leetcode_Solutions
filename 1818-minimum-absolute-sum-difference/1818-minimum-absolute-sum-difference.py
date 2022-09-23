c3716d8764d0c4d1de9707d87048f7daf77d9311
henok3878 <81500872+henok3878@users.noreply.github.com>
2022-09-24 00:07:22 +0300

    Time: 3247 ms (10.34%), Space: 29.2 MB (76.67%) - LeetHub

--- 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.py
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.py
@@ -1,26 +1,15 @@
 class Solution:
     def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
-        nums1_sorted = sorted(nums1)
-        best = [-1] * len(nums2) 
-        for i,num in enumerate(nums2):
-            l = bisect_left(nums1_sorted,num)
-            if l == len(nums2):
-                best[i] = (abs(num - nums1_sorted[l - 1]),abs(num - nums1[i]))
-            elif num == nums1_sorted[l] or l == 0:
-                best[i] = (abs(num - nums1_sorted[l]), abs(num - nums1[i]))
-            else:
-                if abs(num - nums1_sorted[l]) >= abs(num - nums1_sorted[l-1]):
-                    best[i] = (abs(num - nums1_sorted[l-1]),abs(num - nums1[i]))
-                else:
-                    best[i] = (abs(num - nums1_sorted[l]), abs(num - nums1[i]))
+        n = len(nums1)
+        nums1_sorted = sorted(nums1) 
         total = 0 
+        gain = 0 
         for num1,num2 in zip(nums1,nums2):
-            total += abs(num1- num2) 
-        best.sort(key = lambda x: abs(x[0]-x[1]), reverse = True)
-        total -= best[0][1]
-        total += best[0][0]
-        # print(best)
-        
-        return total % (10**9 + 7)
-    
-                
+            l = bisect_left(nums1_sorted,num2) 
+            curr = abs(num2-num1)
+            if l < n:
+                gain = max(gain,curr - abs(num2 - nums1_sorted[l])) 
+            if l > 0:
+                gain = max(gain, curr -abs(num2 - nums1_sorted[l-1]) ) 
+            total += curr
+        return (total - gain) % (10**9 + 7)
