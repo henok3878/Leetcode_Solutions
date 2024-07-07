2aff5613578e2ad656c524df2ce8bf4b3ef1bc67
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-07-07 07:06:48 +0300

    Time: 40 ms (17.64%), Space: 16.5 MB (79.17%) - LeetHub

--- 1642-water-bottles/1642-water-bottles.py
+++ 1642-water-bottles/1642-water-bottles.py
@@ -4,7 +4,8 @@ class Solution:
         empty = 0 
         while numBottles > 0 or empty >= numExchange: 
             ans += numBottles 
-            temp = numBottles 
-            numBottles = ( empty + numBottles) // numExchange 
-            empty = ((empty + temp) % numExchange)
+            empty += numBottles 
+            numBottles = empty // numExchange 
+            empty %= numExchange 
+           
         return ans 
