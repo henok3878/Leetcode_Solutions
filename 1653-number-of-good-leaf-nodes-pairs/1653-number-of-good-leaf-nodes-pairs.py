adf9e9642bb14fe1162840706cbe29299b0e2e60
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-07-19 01:00:37 +0300

    Time: 81 ms (77.2%), Space: 17 MB (23.3%) - LeetHub

--- 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.py
+++ 1653-number-of-good-leaf-nodes-pairs/1653-number-of-good-leaf-nodes-pairs.py
@@ -6,34 +6,32 @@
 #         self.right = right
 class Solution:
     def countPairs(self, root: TreeNode, distance: int) -> int:
+
+        ans = 0
         
-        leaves = []
         def is_leaf(node):
             return node and node.left is None and node.right is None 
-
-        def connect_parent(node, parent):
+            
+        def traverse(node):
             if not node:
-                return 
-            elif node.left is None and node.right is None:
-                leaves.append(node) 
-            node.parent = parent 
-            connect_parent(node.left, node) 
-            connect_parent(node.right, node) 
-        
-        connect_parent(root, None) 
-        ans = 0 
-        for leave in leaves:
-            q = deque() 
-            q.append((leave, 0, None)) 
-            while q:
-                curr, d, p = q.popleft() 
-                if is_leaf(curr) and (0 < d <= distance):
-                    ans += 1 
-                if curr.left and curr.left != p:
-                    q.append((curr.left, d + 1, curr)) 
-                if curr.right and curr.right != p:
-                    q.append((curr.right, d + 1, curr))
-                if curr.parent and curr.parent != p:
-                    q.append((curr.parent, d + 1, curr)) 
+                return defaultdict(int)
 
-        return ans // 2
+            curr = defaultdict(int) 
+            if is_leaf(node):
+                curr[0] = 1 
+            else:
+                left = traverse(node.left) 
+                right = traverse(node.right) 
+                for l in left.keys():
+                    curr[l + 1] = left[l]
+                    for r in right.keys():
+                        if l + r + 2 <= distance:
+                            nonlocal ans
+                            ans += left[l] * right[r] 
+                for k in right.keys():
+                    curr[k + 1] += right[k]
+            # print(node.val, curr)
+            return curr 
+
+        traverse(root)
+        return ans 
