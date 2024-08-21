d1d03289a30fc776b581319bac9c8e8721387f43
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-08-21 04:08:38 +0300

    Time: 240 ms (94.25%), Space: 63.3 MB (96.16%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,36 +1,32 @@
+from random import choice
 class RandomizedSet:
 
     def __init__(self):
-        self.val_idx = {}
-        self.vals = [] 
+        self.nums = []
+        self.num_idx = {}
 
     def insert(self, val: int) -> bool:
-        # print(self.val_idx)
-        if val in self.val_idx:
+        if(val in self.num_idx):
             return False 
         else:
-            self.val_idx[val] = len(self.vals) 
-            self.vals.append(val) 
-            return True 
+            self.num_idx[val] = len(self.nums)
+            self.nums.append(val)
+            return True
         
     def remove(self, val: int) -> bool:
-        if val in self.val_idx:
-            idx = self.val_idx[val]
-            last_el = self.vals[-1]
-            self.vals[-1] = val 
-            self.vals[idx] = last_el 
-            del self.val_idx[val] 
-            if idx != len(self.vals)-1:
-                self.val_idx[last_el] = idx 
-            self.vals.pop() 
+        if(val in self.num_idx):
+            idx = self.num_idx[val] 
+            n = len(self.nums) - 1
+            self.num_idx[self.nums[-1]] = idx
+            del self.num_idx[val]
+            self.nums[n],self.nums[idx] = self.nums[idx], self.nums[n] 
+          
+            self.nums.pop() 
             return True 
         return False 
 
     def getRandom(self) -> int:
-        idx = random.randint(0,len(self.vals)-1) 
-        return self.vals[idx]
-                                        
-        
+        return choice(self.nums)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
