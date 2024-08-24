39a1fe7b1d92d5d62d693ac5d0ac80a33ba851dd
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-08-24 18:09:52 +0300

    Time: 67 ms (5.21%), Space: 16.6 MB (17.39%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,44 +1,17 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symbols = ["M", "D", "C", "L", "X", "V", "I"] 
-        nums = [1000,500, 100, 50, 10, 5, 1] 
-        specials = {4: 'IV', 9: 'IX', 40: 'XL', 90: 'XC', 400: 'CD', 900: 'CM'} 
+        symbols = ["M", "CM", "D", "CD", "C","XC" ,"L","XL" ,"X","IX", "V","IV", "I"] 
+        nums = [1000,900,500,400, 100,90, 50, 40, 10, 9, 5,4, 1] 
         ans = ""
         for i,val in enumerate(nums):
-            # print("num: ", num, end = " ")
+            print("num: ", num)
             if num == 0:
-                break
-            elif(num in specials):
-                ans += specials[num] 
-                # print("curr: ", specials[num])
                 break
             else:
                 q = (num // val) 
                 if(q > 0):
                     x = q * val 
-                    if x in specials:
-                        ans += specials[x] 
-                        # print("curr: ", specials[x])
-                    elif val in [50, 500]:
-                        if val == 500:
-                            qq = num // 100 
-                            xx = qq * 100 
-                            if xx in specials:
-                                ans += specials[xx] 
-                                x = xx
-                            else:
-                                ans += (q * symbols[i])
-                        else:
-                            qq = num // 10 
-                            xx = qq * 10 
-                            if xx in specials:
-                                ans += specials[xx] 
-                                x = xx
-                            else:
-                                ans += (q * symbols[i])
-                    else:
-                        ans += (q * symbols[i])
-                        # print("curr: ", (q * symbols[i]))
+                    ans += (q * symbols[i])
                     num -= x
         return ans 
             
