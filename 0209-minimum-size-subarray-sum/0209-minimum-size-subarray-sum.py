ca330d934cc3f34c3944fcad057dfcedd486e844
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-08-27 01:37:31 +0300

    Time: 205 ms (18.14%), Space: 30.3 MB (21.35%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,33 +1,20 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        
-        def is_pos(x):
-            curr = 0
-            for i in range(x):
-                curr += nums[i] 
-            mx = curr
-            # print("x: ", x, "mx: ", mx)
-            for i in range(1,len(nums)-x + 1):
-                curr -= nums[i-1] 
-                curr += nums[i + x - 1] 
-                # print("curr: ", curr)
-                mx = max(mx, curr) 
-            return target <= mx 
-        
-        l = 1
-        h = len(nums) 
-        best = 0
-        while l <= h:
-            mid = (l + h) // 2 
-            # print("mid: ", mid, "is_pos(mid): ", is_pos(mid))
-            if is_pos(mid):
-                h = mid - 1 
-                best = mid 
+        n = len(nums)
+        l = r = curr = 0
+        ans = float('inf')
+
+        while l < n or r < n:
+
+            if curr < target:
+                if r < n:
+                    curr += nums[r]
+                    r += 1
+                else:
+                    break
             else:
-                l = mid + 1 
-        return best 
-        
-
-
-
-        
+                ans = min(ans, r - l)
+                # print("curr:", curr, "l:", l,"r:",r)
+                curr -= nums[l]
+                l += 1
+        return ans if ans != float('inf') else 0
