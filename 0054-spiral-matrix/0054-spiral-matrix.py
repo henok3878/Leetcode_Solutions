7436eaff62efaae78ab9cdc41affde2c82d3ed96
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-08-28 16:31:09 +0300

    Time: 38 ms (36.51%), Space: 16.6 MB (42.26%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,31 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        def helper(i,j,  n,  m):
+            print("i:", i, "j:", j,"n:", n,"m:",m)
+            if(n == 0 or m == 0): return []
+
+            layer = []
+            # left to right 
+            for jj in range(j, j + m):
+                layer.append(matrix[i][jj])
+
+            # top to bottom 
+            for ii in range(i + 1, i + n -1):
+                layer.append(matrix[ii][j + m - 1])
+            if n > 1:
+                # right to left 
+                for jj in range(j + m-1,j-1,-1):
+                    layer.append(matrix[i + n - 1][jj]) 
+            if m > 1:
+                # bottom to up 
+                for ii in range( i + n - 2,i,-1):
+                    layer.append(matrix[ii][j])
+            
+            new_n = (n - 2) if n > 1 else (n - 1)
+            new_m = (m - 2) if m > 1 else (m - 1)
+            
+            # print("layer:", layer)
+            return layer + helper(i + 1, j + 1, new_n, new_m)
+        
+        return helper(0,0, len(matrix), len(matrix[0]))
