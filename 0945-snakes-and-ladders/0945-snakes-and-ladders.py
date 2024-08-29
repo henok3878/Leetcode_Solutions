4f13cbc24d98e76451b75bcc9e4337c0eb26772e
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-08-29 03:07:53 +0300

    Time: 90 ms (87.78%), Space: 16.7 MB (69.35%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.py
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.py
@@ -7,21 +7,23 @@ class Solution:
             for j in range(n):
                 idx = (i * n) + (j if i % 2 == 0 else (n - 1 - j))
                 l[idx] = board[i][j]
+        
         visited = [False] * (n ** 2 + 1)
-        dist = [-1] * ( n ** 2) 
-        q = deque() 
-        q.append(0)
+        q = deque([0]) 
         visited[0] = True 
-        dist[0] = 0 
+        step = 0
 
         while q:
-            curr = q.popleft() 
-            for nxt in range(curr + 1, min(n**2,curr + 7)):
-                if l[nxt] != -1:
-                    nxt = l[nxt] - 1
-                if not visited[nxt]:
-                    q.append(nxt) 
-                    visited[nxt] = True 
-                    dist[nxt] = dist[curr] + 1
-        return dist[n**2 - 1]
+            size = len(q) 
+            for _ in range(size):
+                curr = q.popleft() 
+                if curr == (n ** 2 - 1):
+                    return step 
+                for nxt in range(curr + 1, min(n**2,curr + 7)):
+                    nxt = l[nxt] - 1 if l[nxt] > -1 else nxt 
+                    if not visited[nxt]:
+                        q.append(nxt) 
+                        visited[nxt] = True 
+            step += 1 
+        return -1
 
