7f015b364251ae979a59904e44f977eb114441f6
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-09-19 04:43:20 +0300

    Time: 44 ms (36.52%), Space: 16.7 MB (32.96%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,15 @@
+class CustomInt:
+    def __init__(self, num):
+        self.num = num 
+    def __lt__(self, other):
+        return str(self.num) + str(other.num) < str(other.num) + str(self.num) 
+    def __str__(self):
+        return str(self.num)
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = [CustomInt(num) for num in nums] 
+        nums.sort(reverse = True) 
+        res = "".join([str(num) for num in nums])
+        if (len(set(res)) == 1 and res[0] == '0'):
+            return "0" 
+        return res 
