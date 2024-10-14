06cd69204c7f4857fdf11f2804f1599632090599
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-10-14 10:51:46 -0400

    Time: 813 ms (20.29%), Space: 17.1 MB (36.2%) - LeetHub

--- 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.py
@@ -1,19 +1,26 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         n = len(bombs)
+        graph = defaultdict(list) 
+        for i in range(n):
+            x,y, r = bombs[i]
+            for j in range(n):
+                if i == j: continue 
+                xx, yy, _ = bombs[j]
+                dist = ((xx - x) ** 2 ) + ((yy - y) ** 2) 
+                if dist <= r ** 2:
+                    graph[i].append(j) 
+
+
         visited = set()
+        
         def helper(i):
             if i >= n or i in visited:
                 return 0
             visited.add(i)
-            x,y, r = bombs[i] 
             total = 1
-            for j in range(n):
-                if i == j: continue 
-                xx, yy, _ = bombs[j]
-                dist = ((xx - x) ** 2  + (yy - y) ** 2)
-                if dist <= r ** 2:
-                    total += helper(j) 
+            for j in graph[i]:
+                total += helper(j) 
             return total 
         
         ans = 0 
