59c9fc274e2aaaa41b954ff4e618d66471589059
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-10-15 07:59:03 -0400

    Time: 34 ms (70.35%), Space: 16.5 MB (74.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,9 +1,24 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums) 
-        dp = [0] * 3 
-        for num in nums:
-            dp[2] = max(dp[0] + num, dp[1]) 
-            dp[0],dp[1] = dp[1], dp[2]
-        return dp[-1]
-        
+        prevs = [0] * 2 
+        for i in range(len(nums)):
+            curr = max(prevs[0] + nums[i], prevs[1]) 
+            prevs[0] = prevs[1] 
+            prevs[1] = curr 
+        return max(prevs) 
+
+
+
+"""
+For each house we have two options: 1. skip 2. rob 
+
+if we rob: curr = can_make[i - 2] + curr
+if we plan to skip: curr = can_make[i-1]
+
+For this house: max(can_make[i - 2] + curr, can_make[i-1])
+
+# we gotta be mindful of the negative indexing for i < 2
+
+Time: O(N) 
+space: O(1)
+"""
