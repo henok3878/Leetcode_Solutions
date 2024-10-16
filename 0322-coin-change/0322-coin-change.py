04fd2fc0427afd2750e1f91b5f71b7b94335916b
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-10-16 09:00:31 -0400

    Time: 944 ms (30.78%), Space: 38.2 MB (12.49%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,39 +1,18 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        @cache 
+        def minCoins(amt):
+            if amt == 0:
+                return 0
+            elif amt < 0:
+                return float('inf') # we can't make 
+            res = float('inf')
+            for coin in coins:
+                res = min(res, minCoins(amt - coin) + 1)
+            return res 
 
-        @lru_cache(None) 
-        def helper(i, amt):
-            if amt == amount:
-                return 0 
-            elif amt > amount or i >= len(coins):
-                return float('inf') 
-            # choose
-            op1 = helper(i, amt + coins[i]) + 1
-            # skip 
-            op2 = helper(i + 1, amt) 
-
-            return min(op1, op2) 
-
-        res = helper(0, 0) 
+        res = minCoins(amount) 
         if res == float('inf'):
-            return -1 
+            return -1
         return res
-            
-    
-
-'''
-We could try to check all the possible subsets with sum <= amount.
-
-(i, amt_sofar): # i represents the curr coin we are considering, amt_sofar is amt made
-
-(i, amt_sofar) -> 1) (i, amt_sofar + coin[i]) # use ith coin 
-                  2) (i + 1, amt_sofar) # skip this coin 
-
-# the number of functions calls: 2 ** (amt * n)
-
-# unique number of function calls: amt * n 
-
-# If we use memoization, we can save: (2**(amt * n)) - (amt * n) number of function calls. 
-
-
-'''
