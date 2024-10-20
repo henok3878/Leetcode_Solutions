f3b9c5b4d4f8a543f61a787229f4c6e1e21e91f8
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-10-19 23:20:34 -0400

    Time: 35 ms (99.77%), Space: 17.7 MB (41.59%) - LeetHub

--- 0838-design-linked-list/0838-design-linked-list.py
+++ 0838-design-linked-list/0838-design-linked-list.py
@@ -21,7 +21,12 @@ class MyLinkedList:
         return temp.val
 
     def addAtHead(self, val: int) -> None:
-        self.addAtIndex(0,val)
+        new_node = Node(val) 
+        new_node.nxt = self.dummy_head.nxt 
+        self.dummy_head.nxt = new_node 
+        self.size += 1
+        if(self.size == 1):
+            self.tail = new_node
 
     def addAtTail(self, val: int) -> None:
         new_node = Node(val) 
