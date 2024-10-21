2961a9345150cbd47e32c831cb794c34abf0533e
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-10-21 14:55:16 -0400

    Time: 7 ms (98.29%), Space: 16.7 MB (5.36%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,38 +1,84 @@
 class Solution:
-    def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        def check(i, row = False):
-            seen = [0] * 10
-            for j in range(9):
-                cell = board[i][j] if row else board[j][i]
-                if cell == ".":
-                    continue
-                cell = int(cell)
-                seen[cell] += 1 
-                if seen[cell] > 1:
-                    return False 
-            return True 
-        
-        def check_sub_box(x,y):
-            seen = [0] * 10 
-            for i in range(3):
-                for j in range(3):
-                    cell = board[x + i][y + j] 
-                    if cell == ".":
-                        continue 
-                    cell = int(cell)
+    def isValidSudoku(self, grid: List[List[str]]) -> bool:
+        n = len(grid) # num of rows 
+        m = len(grid[0]) # num of cols
 
-                    seen[cell] += 1 
-                    if seen[cell] > 1:
-                        return False 
+        def is_subgrid_valid(x,y): 
+            '''
+            Given the start of the subgrid, returns True if it's valid False otherwise. 
+            '''
+            seen = [False] * 10
+            for i in range(x, x + 3):
+                for j in range(y, y + 3):
+                    if grid[i][j] != '.':
+                        val = int(grid[i][j]) 
+                        if seen[val]:
+                            return False 
+                        seen[val] = True 
             return True 
         
-        for i in range(9):
-            if (not check(i)) or (not check(i, True)):
-                # print("First")
-                return False 
-            if (i % 3 == 0):
-                if (not check_sub_box(0,i)) or (not check_sub_box(3, i)) or (not check_sub_box(6, i)):
-                    # print("Second")
+        for i in range(0,n,3):
+            for j in range(0, m, 3):
+                if not is_subgrid_valid(i,j):
                     return False 
-        return True
+        cols_info = [[False] * 10 for _ in range(m)]
+        for i in range(n):    
+            curr_row = [False] * 10 
+            for j in range(m):
+                if grid[i][j] != '.':
+                    val = int(grid[i][j]) 
+                    if curr_row[val] or cols_info[j][val]:
+                        return False 
+                    cols_info[j][val] = True 
+                    curr_row[val] = True 
+        return True 
+
+
+
+
+
+"""
+does jth col, has x? 
+
+m * 10 grid 
+
+
+Given sudoko board, check if it's valid. 
+    - Valid:
+        - 1-9 in a row but no duplicate  
+        - 1-9 in a col but non duplicate 
+        - same for 3x3 boxes 
+
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+
+Clarifications? 
+- Do we consider all 3x3 boxes? for example this one TL: 2,2 -> BR: 4,4
+    No
+- we don't have have all the digits in a row, col, subgrid, right? 
+    Yes
+- Should I assume all the iinputs has size 9x9? Yes
+
+Approaches:
+    1: Brute Force:
+        - For each cell (i, j), 
+            - Validate ith row:
+            - Validate jth col: 
+        - For each subgrid, validate
+            - 
+    2. 
+        - Iterate over the board while storing visited (already seen) values for curr row and all the cols
+            - if curr value is seen, return False 
+        - validate subgrid 
+
+Implementation: 
+- is_subgrid_valid(x,y):
+
+"""
