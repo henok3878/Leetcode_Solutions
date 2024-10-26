18378396e36711f250799c6f637887288bfd125b
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-10-26 10:12:06 -0400

    Time: 3 ms (99.57%), Space: 16.6 MB (78.26%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,20 +2,23 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1" 
-        cnt = self.countAndSay(n - 1) 
-        say = ""
-        prev = ""
-        curr_cnt = 0
-        for i in range(len(cnt)):
-            if cnt[i] == prev:
-                curr_cnt += 1 
-            else:
-                if curr_cnt > 0:
-                    say += f"{curr_cnt}{prev}"
-                curr_cnt = 1
-                prev = cnt[i]
-        if curr_cnt > 0:
-            say += f"{curr_cnt}{prev}"
-        return say 
+        curr = "1"
+        for _ in range(1,n):
+            say = ""
+            prev = ""
+            curr_cnt = 0
+            for i in range(len(curr)):
+                if curr[i] == prev:
+                    curr_cnt += 1 
+                else:
+                    if curr_cnt > 0:
+                        say += f"{curr_cnt}{prev}"
+                    curr_cnt = 1
+                    prev = curr[i]
+            if curr_cnt > 0:
+                say += f"{curr_cnt}{prev}"
+            curr = say
+        
+        return curr
 
         
