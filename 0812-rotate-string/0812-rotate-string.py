7074b7a9cf0ff74bd2a40ab85aa030a8b71a9865
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-11-03 10:57:14 -0500

    Time: 0 ms (100%), Space: 16.6 MB (40.35%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -2,13 +2,4 @@ class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if(len(s) != len(goal)):
             return False
-        for split in range(len(s)):
-            offset = len(s) - (split + 1)
-            valid = True
-            for i in range(len(s)):
-                if s[i] != goal[(i + offset)%len(s)]:
-                    valid = False 
-                    break 
-            if valid:
-                return True
-        return False
+        return s in goal + goal 
