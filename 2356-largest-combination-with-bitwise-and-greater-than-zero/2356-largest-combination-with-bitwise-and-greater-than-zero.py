2484de0f3ca73c3bc3e8b23944f404984a1646ba
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-11-07 18:42:13 -0500

    Time: 581 ms (37.65%), Space: 26.6 MB (96.05%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -1,8 +1,8 @@
 class Solution:
     def largestCombination(self, candidates: List[int]) -> int:
-        cnts = [0] * 32 
+        cnts = [0] * 24
         for num in candidates:
-            for i in range(32):
+            for i in range(24):
                 if num & (1 << i):
                     cnts[i] += 1 
         return max(cnts)
