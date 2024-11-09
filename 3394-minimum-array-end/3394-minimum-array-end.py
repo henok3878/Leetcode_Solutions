f7d6439049a4a31a13a04ef36749538666caa12f
henok3878 <81500872+henok3878@users.noreply.github.com>
2024-11-09 17:27:29 -0500

    Time: 3 ms (44.9%), Space: 16.7 MB (33.08%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.py
+++ 3394-minimum-array-end/3394-minimum-array-end.py
@@ -1,19 +1,13 @@
 class Solution:
     def minEnd(self, n: int, x: int) -> int:
-        '''
-        observations:
-        - x is the first element 
-        - for elem y, the ff should hold (y & x) = x
-        '''
         ans = x
         n -= 1
-        j = 0
-        for i in range(64):
+        for i in range(55):
             if (x >> i) & 1 == 0:
                 # ith bit is zero 
-                if (n & (1 << j)):
+                if (n & 1):
                     ans |= (1 << i)
-                j += 1
+                n >>= 1
         return ans 
 
 
