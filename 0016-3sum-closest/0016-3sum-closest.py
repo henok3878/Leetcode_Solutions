5593c57e1fa3056d23c46f149661811ec555e39a
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-02-19 20:45:16 -0500

    Time: 904 ms (5.01%), Space: 18 MB (9.69%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,26 +1,24 @@
 from sortedcontainers import SortedList 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        best = float('inf')
-        sl = SortedList() 
+        nums.sort() 
         n = len(nums)
+        best = float('inf')
         for i in range(n):
-            for j in range(i + 1, n):
-                curr = nums[i] + nums[j] 
-                best_comp = target - curr 
-                k = bisect_left(sl, best_comp) 
-                if(k < len(sl)):
-                    op1 = curr + sl[k] 
-                    if(abs(target - op1) < abs(target - best)):
-                        best = op1
-                if(k - 1 >= 0):
-                    op2 = curr + sl[k - 1]
-                    if(abs(target - op2) < abs(target - best)):
-                        best = op2
-                if (k + 1 < len(sl)): 
-                    op3 = curr + sl[k + 1]
-                    if(abs(target - op3) < abs(target - best)):
-                        best = op3
-                    
-            sl.add(nums[i])
+            j = 0 
+            k = n - 1 
+            while(j < k):
+                if(j == i):
+                    j += 1 
+                    continue 
+                if (k == i):
+                    k -= 1 
+                    continue 
+                curr = nums[i] + nums[j] + nums[k] 
+                if(abs(curr - target) < abs(best - target)):
+                    best = curr 
+                if curr > target:
+                    k -= 1 
+                else:
+                    j += 1 
         return best 
