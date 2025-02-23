1d87d6545e30d83083c5c5856adc20c6e565a1b1
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-02-22 21:57:58 -0500

    Time: 3 ms (8.3%), Space: 17.9 MB (44.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,17 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p2 = 0
-        for i in range(n + m):
-            if i >= m:
-                nums1[i] = nums2[p2] 
-                p2 += 1 
-            elif p2 < n and  nums1[i] > nums2[p2]:
-                nums1[i], nums2[p2] = nums2[p2], nums1[i] 
-                temp = p2 
-                while temp + 1 < n and nums2[temp] > nums2[temp + 1]:
-                    nums2[temp], nums2[temp + 1] = nums2[temp + 1], nums2[temp] 
-                    temp += 1 
-            # print(nums1, nums2)
-
-     
+        p1 = m - 1 
+        p2 = n - 1 
+        final_pos = n + m - 1 
+        while final_pos >= 0:
+            if p1 >= 0 and p2 >= 0:
+                if(nums1[p1] >= nums2[p2]):
+                    nums1[final_pos] = nums1[p1]
+                    p1 -= 1 
+                else:
+                    nums1[final_pos] = nums2[p2]
+                    p2 -= 1 
+            elif p1 >= 0:
+                nums1[final_pos] = nums1[p1] 
+                p1 -= 1 
+            else:
+                nums1[final_pos] = nums2[p2] 
+                p2 -= 1 
+            final_pos -= 1 
+        
