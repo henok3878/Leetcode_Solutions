098b200ab30746fe542e3a86eb7c76d593acc086
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-02-22 21:14:07 -0500

    Time: 3 ms (8.3%), Space: 17.8 MB (44.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,36 +3,17 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        pt1 = m - 1
-        pt2 = n - 1
-        last_used = (n + m - 1); 
-        while(last_used >= 0):
-            if( (pt1 >= 0 and pt2 < 0) or (pt1 >= 0 and nums1[pt1] >= nums2[pt2])):
-                nums1[last_used] = nums1[pt1]
-                pt1 -= 1
-            else:
-                nums1[last_used] = nums2[pt2]
-                pt2 -= 1 
-            last_used -= 1 
-        
-         
-        '''
-        O(n + m)
-        
-        nums1 = [1, 2, 3, 0, 0, 0] | nums2 = [2, 5, 6] 
-                 ^.                           ^
-        
-        if nums1 ptr value <= nums2 ptr value -> just advance ptr of nums 1 and repeat         
-        nums1 = [4, 5, 6] | nums2 = [2, 3, 4]
-                 ^                   ^ 
-                 2  3                 4
-                                     3 4 4 
-                                     ^ 
-                                     5 
-                                     4 4 5
-                
-              nums1 = 6, 7, 8 | nums2 = 1, 2,3 ,4  
-              
-                    O(n*M): 
-        if nums2 ptr val < nums1 pt val -> 
-        '''
+        p2 = 0
+        for i in range(n + m):
+            if i >= m:
+                nums1[i] = nums2[p2] 
+                p2 += 1 
+            elif p2 < n and  nums1[i] > nums2[p2]:
+                nums1[i], nums2[p2] = nums2[p2], nums1[i] 
+                temp = p2 
+                while temp + 1 < n and nums2[temp] > nums2[temp + 1]:
+                    nums2[temp], nums2[temp + 1] = nums2[temp + 1], nums2[temp] 
+                    temp += 1 
+            # print(nums1, nums2)
+
+     
