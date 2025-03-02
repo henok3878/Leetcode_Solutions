a19360f6a763ab72fc938a133e8af115161fa38d
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-01 21:43:04 -0500

    Time: 135 ms (56.66%), Space: 58.1 MB (11.68%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,29 +1,40 @@
 class RandomizedSet:
 
     def __init__(self):
+        self.seed = 42 
+        self.a = 1103515245
+        self.c = 12345
+        self.m = 2 ** 31 
         self.map = {}
         self.nums = []
 
     def insert(self, val: int) -> bool:
         if val in self.map:
             return False 
-        self.map[val] = len(self.nums)
         self.nums.append(val) 
+        self.map[val] = len(self.nums) - 1 
         return True 
 
     def remove(self, val: int) -> bool:
         if val in self.map:
             idx = self.map[val]
-            self.map[self.nums[-1]] = idx 
-            self.nums[-1], self.nums[idx] = self.nums[idx], self.nums[-1] 
-            self.nums.pop() 
-            self.map.pop(val)
+            self.map[self.nums[-1]] = idx
+            self.nums[-1], self.nums[idx] = self.nums[idx], self.nums[-1]
+            self.nums.pop()  
+            self.map.pop(val) 
             return True 
         return False 
      
 
     def getRandom(self) -> int:
-        return random.choice(self.nums) 
+        return self.random()
+
+    def random(self):
+        n = len(self.nums)
+        self.seed = ((self.a * self.seed) + self.c) % self.m 
+        idx = int((self.seed / self.m) * n)
+        return self.nums[idx]
+
 
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
