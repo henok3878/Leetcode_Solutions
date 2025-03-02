00ff23992e0867a0ebfb909ec8abbb5b0b38078d
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-01 21:14:11 -0500

    Time: 7 ms (10.42%), Space: 23.5 MB (14.44%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -1,21 +1,18 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        best = 0 
-        low = 0
-        high = 10**3 
-
-        def pos(x):
-            cnt = 0 
-            for num in citations:
-                if num >= x:
-                    cnt += 1 
-            return cnt >= x 
-        while low <= high:
-            mid = (low + high) // 2 
-            if pos(mid):
-                best = mid 
-                low = mid + 1 
-            else:
-                high = mid - 1 
-        return best 
-            
+        mx = max(citations) 
+        n = len(citations)
+        counts = [0] * (mx + 1) 
+        for cite in citations:
+            counts[cite] += 1 
+        sorted_citations = [] 
+        for i, cnt in enumerate(counts):
+            if cnt:
+                for _ in range(cnt):
+                    sorted_citations.append(i) 
+        # print(sorted_citations)
+        for i in range(n):
+            rem = (n - i) 
+            if sorted_citations[i] >= rem:
+                return rem 
+        return 0 
