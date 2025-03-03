40dd5048861b8ffcc865c3ff31b04d6137429ba2
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-03 09:02:54 -0500

    Time: 18 ms (47.4%), Space: 19.8 MB (91.76%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -2,24 +2,18 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         n = len(ratings)
         lr = [0] * n 
-        p = float('inf') 
-        incr = 0 
-        for i in range(n):
-            if ratings[i] > p:
-                incr += 1 
+        lr[0] = 1  
+        for i in range(1,n):
+            if ratings[i] > ratings[i-1]:
+                lr[i] = lr[i-1] + 1 
             else:
-                incr = 0
-            p = ratings[i] 
-            lr[i] = incr 
-        incr = 0 
-        p = float('inf')
-        ans = 0
-        for i in range(n-1,-1,-1):
-            if ratings[i] > p:
-                incr += 1 
+                lr[i] = 1 
+        rl = 1 
+        lr[-1] = max(lr[-1], 1) 
+        for i in range(n-2, -1,-1):
+            if ratings[i] > ratings[i + 1]:
+                rl += 1   
             else:
-                incr = 0 
-            p = ratings[i]
-            curr = max(lr[i], incr) 
-            ans += (curr + 1) 
-        return ans
+                rl = 1
+            lr[i] = max(lr[i], rl)
+        return sum(lr)
