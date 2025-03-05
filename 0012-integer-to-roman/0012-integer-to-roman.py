949d22891d0c4ddd1be5bbfe7b3d869cbd474753
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-05 09:34:47 -0500

    Time: 7 ms (44.55%), Space: 17.8 MB (45.8%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,16 +1,10 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        symbols = ["M", "CM", "D", "CD", "C","XC" ,"L","XL" ,"X","IX", "V","IV", "I"] 
-        nums = [1000,900,500,400, 100,90, 50, 40, 10, 9, 5,4, 1] 
-        ans = ""
-        for i,val in enumerate(nums):
-            if num == 0:
-                break
-            else:
-                q = (num // val) 
-                if(q > 0):
-                    x = q * val 
-                    ans += (q * symbols[i])
-                    num -= x
+        mappings = {1:"I",4:"IV", 5:"V", 9:"IX", 10:"X",40:"XL", 50:"L",90:"XC",100:"C",
+        400:"CD", 500:"D", 900: "CM",1000:"M"}
+        ans = ''
+        for k in reversed(mappings.keys()):
+            while num >= k:
+                num -= k 
+                ans += mappings[k]
         return ans 
-            
