547e339673edba371babca4757b5cb84f603ce16
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-05 07:30:50 -0500

    Time: 3 ms (80.2%), Space: 17.7 MB (90.47%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,15 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        mappings = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, 
-            "XC": 90, "C": 100, "CD": 400,  "D": 500,"CM": 900, "M": 1000}
+        mappings = {"I": 1, "V": 5, "X": 10, "L": 50,  "C": 100,  "D": 500, "M": 1000}
         ans = 0
         n = len(s) 
-        st = 0
-        for k in reversed(mappings.keys()):
-            len_k = len(k) 
-            if st >= n:
-                break 
-            while st + len_k <= n and s[st: st + len_k] == k:
-                ans += mappings[k] 
-                st += len_k
+        st = 0 
+        while st < n:
+            val1 = mappings[s[st]] 
+            if st + 1 < n and mappings[s[st + 1]] > val1:
+                ans += (mappings[s[st + 1]] - val1) 
+                st += 1 
+            else:
+                ans += val1 
+            st += 1 
         return ans 
