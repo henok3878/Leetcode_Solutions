47150666002e35e8924591c35b86bd6f830990b3
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-05 07:23:27 -0500

    Time: 13 ms (7.31%), Space: 17.8 MB (40.72%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,17 +1,15 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        symbols_int = {"I": 1, "V": 5, "X" : 10, "L": 50, "C" : 100, "D" : 500, "M" : 1000} 
+        mappings = {"I": 1, "IV": 4, "V": 5, "IX": 9, "X": 10, "XL": 40, "L": 50, 
+            "XC": 90, "C": 100, "CD": 400,  "D": 500,"CM": 900, "M": 1000}
+        ans = 0
         n = len(s) 
-        total = 0
-        i = 0
-        while i < n:
-            curr = symbols_int[s[i]]
-            if i < (n - 1) and symbols_int[s[i + 1]] > curr: 
-                total += (symbols_int[s[i + 1]] - curr) 
-                # print(symbols_int[s[i + 1]] - curr)
-                i += 1
-            else:
-                total += curr
-                # print(curr)
-            i += 1
-        return total 
+        st = 0
+        for k in reversed(mappings.keys()):
+            len_k = len(k) 
+            if st >= n:
+                break 
+            while st + len_k <= n and s[st: st + len_k] == k:
+                ans += mappings[k] 
+                st += len_k
+        return ans 
