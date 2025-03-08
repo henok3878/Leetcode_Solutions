ecf56e83748cf2d70d94448c9382112025b1129b
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-07 23:51:50 -0500

    Time: 0 ms (100%), Space: 17.9 MB (10.63%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,4 +1,52 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        idx = haystack.find(needle) 
-        return idx
+
+        mod = int(1e9) + 7
+        p = 31 
+        inv_p = pow(p, mod - 2, mod) 
+
+        n = len(haystack)
+        m = len(needle)
+        if n < m:
+            return -1 
+            
+        def idx(ch):
+            return ord(ch) - ord('a') 
+        
+
+        def hash(word):
+            val = 0
+            pol = 1
+            for ch in word:
+                ch_num = idx(ch) 
+                val = (val + (ch_num * pol) % mod ) % mod 
+                pol = (pol * p) % mod 
+            return val 
+
+        def check(st):
+            for i in range(m):
+                if needle[i] != haystack[i + st]:
+                    return False 
+            return True 
+
+        req = hash(needle) 
+        curr = 0
+        pol = 1 
+        for i in range(m):
+            ch_num = idx(haystack[i])
+            curr = (curr + (ch_num * pol) % mod) % mod 
+            pol = (pol * p) % mod 
+        
+        if curr == req and check(0):
+            return 0  
+        pol = (pol * inv_p) % mod 
+        for i in range(m, n):
+            curr = (curr - idx(haystack[i-m])) % mod 
+            curr = (curr * inv_p) % mod 
+            curr = (curr + (pol * idx(haystack[i])) % mod) % mod 
+            if curr == req and check(i - m + 1): 
+                return i - m + 1 
+        return -1 
+         
+
+            
