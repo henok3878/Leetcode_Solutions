8a77a10d9887307b25ce8c62c5b383e179e4507d
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-20 21:55:50 -0400

    Time: 1372 ms (95.04%), Space: 20.2 MB (94.61%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,23 +1,33 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        n = len(nums)
         nums.sort() 
-        n = len(nums) 
-        ans = [] 
-        i = 0
-        while i < n - 2:
-            target = -(nums[i]) 
-            j = i + 1
-            k = n-1 
+        i = 0 
+        def nxt(nums, i, dir = 1):
+            n = len(nums) 
+            while i + dir >= 0 and i + dir < n and nums[i + dir] == nums[i]:
+                i += dir 
+            return i + dir 
+
+        while(i < n):
+            num1 = nums[i] 
+            j = i + 1 
+            k = n - 1 
+            # num1 + num2 + num3 = 0 
+            # num2 + num3 = -num1 
+            target = -1 * num1 
             while (j < k):
-                val = (nums[j] + nums[k]) 
-                if(val > target):
-                    k -= 1 
-                elif (val < target):
-                    j += 1 
+                num2 = nums[j] 
+                num3 = nums[k] 
+                if(num2 + num3 > target):
+                    k = nxt(nums, k, dir = -1)
+                elif (num2 + num3 < target):
+                    j = nxt(nums, j)
                 else:
-                    ans.append([nums[i], nums[j], nums[k]])
-                    while(j < k and nums[j] == nums[j + 1]): j+=1 
-                    j += 1 
-            while(i < n-1 and nums[i] == nums[i + 1]): i += 1 
-            i += 1 
+                    ans.append((num1, num2, num3)) 
+                    j = nxt(nums, j)
+            i = nxt(nums, i) 
         return ans 
+
+# -4, -1, -1, 0, 1, 2, 
