40f68230107ad5ddf0af224ce87ade3f9c954581
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-03-29 21:38:35 -0400

    Time: 459 ms (13.57%), Space: 18.3 MB (56.1%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,72 +1,36 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        n = len(s) 
-        k = len(t) 
-        if k > n:
-            return "" 
-        dict_t = defaultdict(int) 
-        for c in t:
-            dict_t[c] += 1 
-        
-        def check(curr):
-            for k in dict_t:
-                if curr[k] < dict_t[k]:
+        m, n = len(s), len(t) 
+        count_t = Counter(t) 
+        left = 0 
+        l = float('inf')
+        st = 0
+        count_curr = Counter() 
+        def check(counter1, counter2):
+            for k, v  in counter2.items():
+                if counter1[k] < v:
                     return False 
             return True 
-        curr = defaultdict(int) 
-        l = 0 
-        ans = float('inf')
-        st = -1
-        for r in range(n):
-            curr[s[r]] += 1 
-            while l <= r and check(curr):
-                # print("l:", l, "r:", r, "s[l:r]", s[l:r + 1])
-                curr_l = r - l + 1 
-                if curr_l < ans:
-                    ans = curr_l 
-                    st = l
-                curr[s[l]] -= 1 
-                l += 1 
-        
-        if ans == float('inf'):
-            return "" 
-        return s[st:st + ans]
             
-             
-
-        
-
-"""
-Chars:  
-    - Upper & lower case letters
-    - 52 chars in total (26 each) 
-
-len(s), len(t) <= 10**5 
-
-
-N = len(s)
-M = len(t) 
-
-if M > N:
-    no way 
-else:
-    # O(N * 52) 
-
-- each index in s has a potential to be the start of our answer string and we have n such indices 
-
-- for each index in s: #N
-    - check if there is a substring starting here that contains t, if so return the len #M 
-    
-___________________________________________
-^***********
- ^***********
-
-if we check for index i, using O(M) time, find a way to check for (i + 1) in O(1) time 
-
-brute force: O(N*M)
-
-
-
-
-
-"""
+        for right in range(m):
+            ch = s[right] 
+            count_curr[ch] += 1 
+            if check(count_curr, count_t):
+                curr_len = right - left + 1 
+                if curr_len <= l:
+                    st = left 
+                    l = curr_len 
+                # print(f"l: {left}, r: {right}, curr_cnt: {count_curr}")
+                count_curr[s[left]] -= 1 
+                left += 1 
+                while(left <= right and check(count_curr, count_t)):
+                    curr_len = right - left + 1 
+                    if curr_len <= l:
+                        st = left 
+                        l = curr_len 
+                    count_curr[s[left]] -= 1 
+                    left += 1 
+                
+        if l == float('inf'):
+            return ""
+        return s[st: st + l] 
