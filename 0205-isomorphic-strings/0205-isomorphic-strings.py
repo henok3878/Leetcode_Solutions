3f078c6dc4f791b697e16822adbdb22ca7afc2d6
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-04-14 20:57:31 -0400

    Time: 7 ms (56.92%), Space: 17.8 MB (95.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,15 +1,15 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         mappings = {} 
-        seen = set() 
+        already_used = set() 
         for i, ch in enumerate(s):
             ch2 = t[i]
             if ch in mappings:
                 if mappings[ch] != ch2:
                     return False 
             else:
-                if ch2 in seen:
+                if ch2 in already_used:
                     return False 
-                seen.add(ch2)
+                already_used.add(ch2)
                 mappings[ch] = ch2
         return True 
