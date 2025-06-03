484af95fdf591703dd1d605d31f16fc1e69216e9
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-06-03 13:57:09 -0400

    Time: 15 ms (77.78%), Space: 28.2 MB (65.13%) - LeetHub

---
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.py
@@ -0,0 +1,27 @@
+from heapq import heappop as pop, heappush as push 
+class Solution:
+    def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
+
+        pq = []
+        can_open = set() 
+
+        def get_state(box):
+            return -(status[box] + int(box in can_open))
+        for box in initialBoxes:
+            push(pq,(get_state(box), box)) 
+            
+        ans = 0
+        visited = set()
+        while pq: 
+            # print(f"pq: {pq}")
+            state , curr = pop(pq) 
+            state = get_state(curr)  
+            if not state:
+                continue 
+            ans += candies[curr]
+            visited.add(curr)
+            can_open.update(keys[curr])
+            for box in containedBoxes[curr]:
+                push(pq, (get_state(box), box))
+                
+        return ans  
